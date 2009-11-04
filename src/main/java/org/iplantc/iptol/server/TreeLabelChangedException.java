bcdec35ae2c91d8f81d37db38084fdeef31e4f11
Evan Deaubl <evan@iplantcollaborative.org>
2009-11-04 09:47:06 -0700

    Added configuration for new tree service to discovery environment

---
+++ src/main/java/org/iplantc/iptol/server/TreeLabelChangedException.java
@@ -0,0 +1,24 @@
+package org.iplantc.iptol.server;
+
+public class TreeLabelChangedException extends Exception {
+
+	public TreeLabelChangedException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public TreeLabelChangedException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public TreeLabelChangedException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public TreeLabelChangedException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
