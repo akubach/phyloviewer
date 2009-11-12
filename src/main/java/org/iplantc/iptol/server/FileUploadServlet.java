219798d7e82eaf79e207599fbcdf412989363dec
sriram srinivasan <sriram@dhcp-96870a20.ece.Arizona.EDU>
2009-11-12 12:44:24 -0700

    merged

--- src/main/java/org/iplantc/iptol/server/FileUploadServlet.java
+++ src/main/java/org/iplantc/iptol/server/FileUploadServlet.java
@@ -10,25 +10,27 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
 import gwtupload.server.UploadAction;
+import gwtupload.server.UploadServlet;
 import gwtupload.server.exceptions.UploadActionException;
+import gwtupload.server.exceptions.UploadException;
 
 import org.apache.commons.fileupload.FileItem;
 import org.mule.MuleServer;
 
 import net.sf.json.JSONObject;
+
 /**
- * A class to accept files from the client. 
- * This class extends the UploadAction class provided by the 
- * GWT Upload library 
+ * A class to accept files from the client. This class extends the UploadAction
+ * class provided by the GWT Upload library
+ * 
  * @author sriram
- *
+ * 
  */
 public class FileUploadServlet extends UploadAction {
-	
-	public final long MAX_FILE_SIZE =  3145728;
-	
-	public final int UPLOAD_DELAY = 0;
-	
+
+	public static final long MAX_FILE_SIZE = 3145728;
+
+	public static final int UPLOAD_DELAY = 0;
 
 	/**
 	 * This method is automatically called for file upload request
@@ -39,36 +41,42 @@ public class FileUploadServlet extends UploadAction {
 	
 	@SuppressWarnings("unchecked")
 	@Override
-	public String executeAction(HttpServletRequest request, List <FileItem> fileItems)
-			throws UploadActionException {
-		
+	public String executeAction(HttpServletRequest request,
+			List<FileItem> fileItems) throws UploadActionException {
+
 		this.maxSize = MAX_FILE_SIZE;
 		this.uploadDelay = UPLOAD_DELAY;
+		String filetype = null;
 		
 		Map map = new HashMap();
 		JSONObject json = null;
-		
-		for(FileItem item: fileItems){
-			if(!item.isFormField()) {
+		for (FileItem item : fileItems) {
+			if (!item.isFormField()) {
 				try {
 					String contents = item.getString();
 					fileUploadedEvent.fileUploaded(contents, item.getName());
 					map.put("file_name",item.getName());
 					map.put("date", (new Date()).toString());
-					map.put("label",item.getName());
+					map.put("label", item.getName());
 					json = JSONObject.fromObject(map);
-					System.out.println("filename ==>" + item.getName());
-				} catch (Throwable t) {
-					throw new UploadActionException(t.getMessage());
+					System.out.println("filename ==>" + item.getName()
+							+ " size ==>" + item.getSize());
+				} catch (Exception e) {
+					e.printStackTrace();	
+				}
+			} else {
+				if(item.getFieldName().equals("file-type")) {
+					filetype =  new String(item.get());
 				}
 			}
 		}
-		removeSessionFileItems(request,false);
-		if(json!= null)
+		//remove files from session. this avoids duplicate submissions
+		removeSessionFileItems(request, false);
+		if (json != null)
 			return json.toString();
 		else
 			return null;
-				
+
 	}
 
 	@Override
