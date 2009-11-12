00258cd0afc09d42badf427ee493358ef307d094
sriram srinivasan <sriram@dhcp-96870a20.ece.Arizona.EDU>
2009-11-12 12:31:20 -0700

    GWT i18N constatns interface

---
+++ src/main/java/org/iplantc/iptol/client/IptolConstants.java
@@ -0,0 +1,11 @@
+package org.iplantc.iptol.client;
+
+import com.google.gwt.i18n.client.Constants;
+
+public interface IptolConstants extends Constants {
+
+	int statusInterval();
+	String noFiles();
+	String fileUploadSuccess();
+	String fileUploadFailed();
+}
