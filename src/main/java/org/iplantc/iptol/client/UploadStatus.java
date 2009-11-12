3c42354cd3c89f51e5ecdb33b612fa9efecc2b89
sriram srinivasan <sriram@dhcp-96870a20.ece.Arizona.EDU>
2009-11-12 12:34:00 -0700

    extend default Baseupload class to get rid of long/unwanted alerts

---
+++ src/main/java/org/iplantc/iptol/client/UploadStatus.java
@@ -0,0 +1,47 @@
+package org.iplantc.iptol.client;
+
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+import gwtupload.client.BaseUploadStatus;
+
+public class UploadStatus extends BaseUploadStatus{
+	protected PopupPanel box = new PopupPanel(false, true);
+	
+	public UploadStatus() {
+		super();
+		super.getWidget().addStyleName("upld-status");
+		box.add(super.getWidget());
+		DOM.setElementAttribute((com.google.gwt.user.client.Element)box.getElement().getFirstChild(), "class", "GWTUpld");
+  }
+	
+	/**
+	 * Returns an empty html widget, 
+	 * so, PopupPanel will never attached to the document by the user
+	 * and it will be attached when show() is called
+	 */
+	@Override public Widget getWidget() {
+		return new HTML();
+	};
+	
+	/**
+	 * show/hide the modal dialog
+	 */
+	@Override
+	public void setVisible(boolean b) {
+		if (b)
+		  box.center();
+		else
+			box.hide();
+	}
+	
+	/**
+	 * eliminate unwanted/lengthy pop-up alerts
+	 */
+	@Override
+	 public void setError(String msg) {
+		    setStatus(Status.ERROR);
+	 }
+}
