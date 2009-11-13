a3c44a869ba001a780522d0db404001859de363e
sriram srinivasan <sriram@dhcp-96870a20.ece.Arizona.EDU>
2009-11-13 16:31:00 -0700

    use ext-gwt widgets

--- src/main/java/org/iplantc/iptol/client/TreeFilesManager.java
+++ src/main/java/org/iplantc/iptol/client/TreeFilesManager.java
@@ -4,22 +4,33 @@ import gwtupload.client.IUploader;
 import gwtupload.client.SingleUploader;
 import gwtupload.client.IUploadStatus.Status;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.extjs.gxt.ui.client.Style.HorizontalAlignment;
+import com.extjs.gxt.ui.client.data.JsonLoadResultReader;
+import com.extjs.gxt.ui.client.data.ModelData;
+import com.extjs.gxt.ui.client.data.ModelType;
+import com.extjs.gxt.ui.client.store.ListStore;
+import com.extjs.gxt.ui.client.widget.ContentPanel;
+import com.extjs.gxt.ui.client.widget.Dialog;
+import com.extjs.gxt.ui.client.widget.grid.ColumnConfig;
+import com.extjs.gxt.ui.client.widget.grid.ColumnModel;
+import com.extjs.gxt.ui.client.widget.grid.Grid;
+import com.extjs.gxt.ui.client.widget.layout.FitLayout;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONParser;
 import com.google.gwt.json.client.JSONValue;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Hidden;
 import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.RadioButton;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -32,12 +43,15 @@ public class TreeFilesManager implements EntryPoint {
 	
 	public static final String operationsRadioGroup = "OPERATIONS_RADIO_GROUP";
 	public static final String filesRadioGroup = "FILES_RADIO_GROUP";
+	ListStore<ModelData> store = null;
+    Grid<ModelData> grid = null;
+    ModelType type = new ModelType();  
+	
 
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-	
 	
 	Button send = new Button("Upload");
 
@@ -45,38 +59,50 @@ public class TreeFilesManager implements EntryPoint {
 	HorizontalPanel hUpload_panel = new HorizontalPanel();
 	SingleUploader defaultUploader = new SingleUploader(new UploadStatus(),
 			send);
-	
+	SingleUploader.setStatusInterval(constants.statusInterval());
 	defaultUploader.add(new Hidden("file-type", "tree"));
 	hUpload_panel.add(defaultUploader);
 	hUpload_panel.add(send);
-	SingleUploader.setStatusInterval(constants.statusInterval());
-
-	VerticalPanel vFiles_panel = new VerticalPanel();
 	defaultUploader.addStyleName("uploadPanel");
 	defaultUploader.setServletPath("servlet.gupld");
-
-	// create dynamic table to display list of uploaded files
-	list_files = new FlexTable();
-	list_files.setText(0, 0, "Select");
-	list_files.setText(0, 1, "Label");
-	list_files.setText(0, 2, "File Name");
-	list_files.setText(0, 3, "Date / Time Uploded");
-	list_files.getRowFormatter().addStyleName(0, "fileListHeader");
-	list_files.addStyleName("fileList");
-	list_files.setCellPadding(5);
-
-	/* TODO - Call a service to retrieve a list of files already uploaded */
-
-	if (list_files.getRowCount() == 1) {
-		list_files.getFlexCellFormatter().setColSpan(1, 0, 3);
-		list_files.setText(1, 0, constants.noFiles());
-	}
-
-	vFiles_panel.add(list_files);
-	HTML prompt = new HTML("Upload your tree: ");
-	prompt.addStyleName("uploadTextAlign");
 	defaultUploader.addOnFinishUploadHandler(onFinishUploaderHandler);
 
+	HTML prompt = new HTML("Upload your tree: ");
+	prompt.addStyleName("uploadTextAlign");
+
+	//column config for tables
+	List<ColumnConfig> columns = new ArrayList<ColumnConfig>(); 
+	columns.add(new ColumnConfig("name","File Name",100));
+	columns.add(new ColumnConfig("label","Label",100));
+	columns.add(new ColumnConfig("date","Uploaded Date/Time",100));
+	
+	// create the column model  
+	 ColumnModel column_model = new ColumnModel(columns);  
+	
+	// defines the xml structure  
+	type.addField("File Name", "file_name");
+	type.addField("Label", "label");
+	type.addField("Uploaded Date/Time","date");
+	
+	
+	//TODO - call service to return a list of files uploaded
+	
+     store = new ListStore<ModelData>();  
+     grid = new Grid<ModelData>(store, column_model);  
+     grid.setBorders(true);  
+     grid.setLoadMask(true);  
+     grid.getView().setEmptyText(constants.noFiles());  
+     
+	 ContentPanel panel = new ContentPanel();  
+	 panel.setFrame(true);  
+	 panel.setCollapsible(true);  
+	 panel.setAnimCollapse(false);  
+	 panel.setButtonAlign(HorizontalAlignment.CENTER);  
+	 panel.setHeading("Uploaded Trees");  
+	 panel.setLayout(new FitLayout());  
+	 panel.add(grid);  
+	 panel.setSize(350, 250);
+	
 	HorizontalPanel hOperations_panel = new HorizontalPanel();
 	hOperations_panel.add(new RadioButton(operationsRadioGroup,"View Tree"));
 	hOperations_panel.add(new RadioButton(operationsRadioGroup,
@@ -86,7 +112,7 @@ public class TreeFilesManager implements EntryPoint {
 
 	RootPanel.get("file_upload").add(prompt);
 	RootPanel.get("file_upload").add(hUpload_panel);
-	RootPanel.get("file_management").add(vFiles_panel);
+	RootPanel.get("file_management").add(panel);
 	RootPanel.get("file_operations").add(hOperations_panel);
 
 }
@@ -98,59 +124,31 @@ private IUploader.OnFinishUploaderHandler onFinishUploaderHandler = new IUploade
 	public void onFinish(IUploader uploader) {
 		// construct a popup to show success / failure message
 		HTML msg = null;
-		final PopupPanel popup = new PopupPanel(false);
-		Button btn_Ok = new Button("Ok");
-		btn_Ok.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				popup.hide();
-			}
-		});
-
-		VerticalPanel vPopupHeaderPanel = new VerticalPanel();
-		HTML headerText = new HTML("File Upload");
-		headerText.addStyleName("popup_header_text");
-		vPopupHeaderPanel.setStyleName("popup_header_panel");
-		vPopupHeaderPanel.add(headerText);
-
-		VerticalPanel vPopupContentPanel = new VerticalPanel();
-		HorizontalPanel holder = new HorizontalPanel();
-		holder.setHorizontalAlignment(HorizontalPanel.ALIGN_CENTER);
-		holder.add(btn_Ok);
-		holder.addStyleName("popup_ok_btn_panel");
-
+		final Dialog popup_dialog = new Dialog();
+		popup_dialog.setHeading("File Upload");
+		popup_dialog.setButtons(Dialog.OK);
+		popup_dialog.setHideOnButtonClick(true);
 		if (uploader.getStatus() == Status.SUCCESS) {
-			JSONValue value = JSONParser
-					.parse(uploader.getServerResponse());
-			JSONObject json = value.isObject();
-			if (json != null) {
-				int row = list_files.getRowCount();
-				if (row == 2 && constants.noFiles().equals(list_files.getText(1, 0))) {
-					row = 1;
-					list_files.getFlexCellFormatter().setColSpan(1, 0, 1);
-				}
-				list_files.setWidget(row, 0, new RadioButton(
-						filesRadioGroup));
-				list_files.setText(row, 1, json.get("label").isString()
-						.stringValue());
-				list_files.setText(row, 2, json.get("file_name").isString()
-						.stringValue());
-				list_files.setText(row, 3, json.get("date").isString()
-						.stringValue());
-			}
+			String response = uploader.getServerResponse();
+//			Window.alert(response);
+//			JSONValue value = JSONParser
+//					.parse(response);
+//			JSONObject json = value.isObject();
+//			if (json != null) {
+//			
+//			}
+			JsonLoadResultReader<ModelData> reader = new JsonLoadResultReader<ModelData>(type);
+			store.add(reader.read(null, response));
 			msg = new HTML(constants.fileUploadSuccess());
 			msg.addStyleName("success");
+			popup_dialog.addText(constants.fileUploadSuccess());
 		} else {
 			msg = new HTML(constants.fileUploadFailed());
 			msg.addStyleName("failure");
+			popup_dialog.addText(constants.fileUploadFailed());
+			
 		}
-
-		vPopupContentPanel.add(vPopupHeaderPanel);
-		vPopupContentPanel.add(msg);
-		vPopupContentPanel.add(holder);
-
-		popup.add(vPopupContentPanel);
-		popup.setStyleName("popup");
-		popup.center();
+		popup_dialog.show();
 	}
 };
 
