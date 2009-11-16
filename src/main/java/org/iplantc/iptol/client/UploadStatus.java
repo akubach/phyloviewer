9dcc4b9f293dedfca15dc0966ef91fab27798c2a
sriram srinivasan <sriram@dhcp-96870a20.ece.Arizona.EDU>
2009-11-16 16:22:22 -0700

    clean up

--- src/main/java/org/iplantc/iptol/client/UploadStatus.java
+++ src/main/java/org/iplantc/iptol/client/UploadStatus.java
@@ -7,41 +7,42 @@ import com.google.gwt.user.client.ui.Widget;
 
 import gwtupload.client.BaseUploadStatus;
 
-public class UploadStatus extends BaseUploadStatus{
+public class UploadStatus extends BaseUploadStatus {
 	protected PopupPanel box = new PopupPanel(false, true);
-	
+
 	public UploadStatus() {
 		super();
 		super.getWidget().addStyleName("upld-status");
 		box.add(super.getWidget());
-		DOM.setElementAttribute((com.google.gwt.user.client.Element)box.getElement().getFirstChild(), "class", "GWTUpld");
-  }
-	
+		DOM.setElementAttribute((com.google.gwt.user.client.Element) box
+				.getElement().getFirstChild(), "class", "GWTUpld");
+	}
+
 	/**
-	 * Returns an empty html widget, 
-	 * so, PopupPanel will never attached to the document by the user
-	 * and it will be attached when show() is called
+	 * Returns an empty html widget, so, PopupPanel will never attached to the
+	 * document by the user and it will be attached when show() is called
 	 */
-	@Override public Widget getWidget() {
+	@Override
+	public Widget getWidget() {
 		return new HTML();
 	};
-	
+
 	/**
 	 * show/hide the modal dialog
 	 */
 	@Override
 	public void setVisible(boolean b) {
 		if (b)
-		  box.center();
+			box.center();
 		else
 			box.hide();
 	}
-	
+
 	/**
 	 * eliminate unwanted/lengthy pop-up alerts
 	 */
 	@Override
-	 public void setError(String msg) {
-		    setStatus(Status.ERROR);
-	 }
+	public void setError(String msg) {
+		setStatus(Status.ERROR);
+	}
 }
