562863d3583674ca017e5ba805d3450cbaba5c67
sriram srinivasan <sriram@dhcp-96870a20.ece.Arizona.EDU>
2009-11-23 10:06:50 -0700

    refactor code

--- src/main/java/org/iplantc/iptol/client/ApplicationLayout.java
+++ src/main/java/org/iplantc/iptol/client/ApplicationLayout.java
@@ -5,8 +5,14 @@ import com.extjs.gxt.ui.client.Style.Scroll;
 import com.extjs.gxt.ui.client.util.Margins;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
 import com.extjs.gxt.ui.client.widget.Viewport;
+import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayout;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayoutData;
+import com.extjs.gxt.ui.client.widget.layout.CenterLayout;
+import com.extjs.gxt.ui.client.widget.menu.Menu;
+import com.extjs.gxt.ui.client.widget.menu.MenuItem;
+import com.extjs.gxt.ui.client.widget.toolbar.ToolBar;
+import com.google.gwt.user.client.ui.Image;
 
 public class ApplicationLayout extends Viewport {
 	
@@ -15,45 +21,76 @@ public class ApplicationLayout extends Viewport {
      ContentPanel center = new ContentPanel();
      ContentPanel east = new ContentPanel();  
      ContentPanel south = new ContentPanel(); 
-     
-	public ApplicationLayout() {
+     private final BorderLayout layout;
+	
+     public ApplicationLayout() {
 		//build top level layout
-		 final BorderLayout layout = new BorderLayout();  
+    	 layout  = new BorderLayout();  
 	     setLayout(layout);  
-	   
-	     center.setScrollMode(Scroll.AUTOX);  
-	   
-	     BorderLayoutData northData = new BorderLayoutData(LayoutRegion.NORTH, 100);  
-	     northData.setCollapsible(true);  
-	     northData.setFloatable(true);  
-	     northData.setHideCollapseTool(true);  
-	     northData.setSplit(true);  
-	     northData.setMargins(new Margins(5, 5, 0, 5));  
+	     BorderLayoutData northData = new BorderLayoutData(LayoutRegion.NORTH, 175);  
+	     northData.setCollapsible(false);  
+	     northData.setFloatable(false);  
+	     northData.setHideCollapseTool(false);  
+	     northData.setSplit(false);  
+	     northData.setMargins(new Margins(0, 0, 0, 0)); 
+	     north.setHeaderVisible(false);
 	   
 	     BorderLayoutData westData = new BorderLayoutData(LayoutRegion.WEST, 150);  
-	     westData.setSplit(true);  
+	     westData.setSplit(false);  
 	     westData.setCollapsible(true);  
 	     westData.setMargins(new Margins(5));  
-	   
+	    
 	     BorderLayoutData centerData = new BorderLayoutData(LayoutRegion.CENTER);  
-	     centerData.setMargins(new Margins(5, 0, 5, 0));  
-	   
+	     centerData.setMargins(new Margins(5, 0, 5, 0)); 
+	     center.setBodyStyle("padding: 25px");
+	     center.setScrollMode(Scroll.AUTOX); 
+	     center.setHeaderVisible(true);
+	     centerData.setCollapsible(false);  
+	     centerData.setFloatable(false);  
+	     centerData.setHideCollapseTool(false);  
+	     centerData.setSplit(false);  
+	     
 	     BorderLayoutData eastData = new BorderLayoutData(LayoutRegion.EAST, 150);  
-	     eastData.setSplit(true);  
+	     eastData.setSplit(false);  
 	     eastData.setCollapsible(true);  
 	     eastData.setMargins(new Margins(5));  
 	   
-	     BorderLayoutData southData = new BorderLayoutData(LayoutRegion.SOUTH, 100);  
-	     southData.setSplit(true);  
-	     southData.setCollapsible(true);  
-	     southData.setFloatable(true);  
-	     southData.setMargins(new Margins(0, 5, 5, 5));  
+	     BorderLayoutData southData = new BorderLayoutData(LayoutRegion.SOUTH, 50);  
+	     southData.setSplit(false);  
+	     southData.setCollapsible(false);  
+	     southData.setFloatable(false);  
+	     southData.setMargins(new Margins(0, 0, 0, 0)); 
+	     south.setHeaderVisible(false);
 	   
 	     add(north, northData);  
 	     add(west, westData);  
 	     add(center, centerData);  
 	     add(east, eastData);  
-	     add(south, southData); 
-	  
+	     add(south, southData);
+	     
+	     //Add basic tool bar
+	     ToolBar toolBar = new ToolBar(); 
+	     toolBar.setBorders(true);
+	        
+	     Button upload = new Button("<b>Upload</b>");
+	     
+	     //create basic menu and menu item
+	     Menu menu = new Menu();
+	     menu.add(new MenuItem("Tree Data"));
+	     
+	     upload.setMenu(menu);
+	     toolBar.add(upload);
+	     
+	     //add our logo...This should be changed to DE logo later
+	     Image logo = new Image("/discoveryenvironment/images/iplant62008-long.jpg");
+	     logo.setHeight("145px");
+	     north.add(logo);
+	     // add to north panel
+	     north.add(toolBar);
+	     
+	}
+	
+	public void hideRegion(LayoutRegion region) {
+		layout.hide(region);
 	}
 }
