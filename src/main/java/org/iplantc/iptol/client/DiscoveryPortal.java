abc4740919e16250ce6b5f1a86b386267c8e381a
sriram srinivasan <sriram@dhcp-96870a20.ece.arizona.edu>
2009-12-07 12:48:20 -0700

    portal for discovery environment

---
+++ src/main/java/org/iplantc/iptol/client/DiscoveryPortal.java
@@ -0,0 +1,16 @@
+package org.iplantc.iptol.client;
+
+import com.extjs.gxt.ui.client.widget.custom.Portal;
+/**
+ * 
+ * @author sriram
+ * A class that extends GXT portal. This is used to track the portlets contained with-in the portal.
+ * Provides customization ability
+ */
+public class DiscoveryPortal extends Portal{
+
+	public DiscoveryPortal(int numColumns) {
+		super(numColumns);
+	}
+	
+}
