5e741b42fb4fa498809bb508e7fe061a0b77f8a1
sriram srinivasan <sriram@dhcp-96870a20.ece.arizona.edu>
2009-12-07 12:29:39 -0700

    refactor code. add newmethods for hiding regions and adding portal to the layout

--- src/main/java/org/iplantc/iptol/client/ApplicationLayout.java
+++ src/main/java/org/iplantc/iptol/client/ApplicationLayout.java
@@ -6,6 +6,7 @@ import com.extjs.gxt.ui.client.event.MenuEvent;
 import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.util.Margins;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
+import com.extjs.gxt.ui.client.widget.HorizontalPanel;
 import com.extjs.gxt.ui.client.widget.Viewport;
 import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayout;
@@ -16,93 +17,140 @@ import com.extjs.gxt.ui.client.widget.toolbar.ToolBar;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Image;
-
+import com.google.gwt.user.client.ui.Widget;
+ 
+/**
+ * 
+ * @author sriram
+ * This class draws the layout for the discovery env.
+ */
+@SuppressWarnings("unused")
 public class ApplicationLayout extends Viewport {
 	
 	IptolConstants constants = (IptolConstants) GWT
 	.create(IptolConstants.class);
-	 ContentPanel north = new ContentPanel();  
-     ContentPanel west = new ContentPanel();  
-     ContentPanel center = new ContentPanel();
-     ContentPanel east = new ContentPanel();  
-     ContentPanel south = new ContentPanel(); 
-     private final BorderLayout layout;
 	
-     public ApplicationLayout() {
+	 ContentPanel north ;  
+	 ContentPanel west;  
+	 ContentPanel center;
+	 ContentPanel east;  
+	 ContentPanel south;
+	 
+	 BorderLayoutData northData;
+	 BorderLayoutData westData;
+	 BorderLayoutData centerData;
+	 BorderLayoutData eastData;
+	 BorderLayoutData southData;
+	 
+	 private final BorderLayout layout;
+	
+     
+    public ApplicationLayout() {
+    	 
 		//build top level layout
     	 layout  = new BorderLayout();  
 	     setLayout(layout);  
-	     BorderLayoutData northData = new BorderLayoutData(LayoutRegion.NORTH, 175);  
+	     
+	     north = new ContentPanel();  
+	     west = new ContentPanel();  
+	     center = new ContentPanel();
+	     east = new ContentPanel();  
+	     south = new ContentPanel();
+	     
+	     northData = new BorderLayoutData(LayoutRegion.NORTH, 155);  
 	     northData.setCollapsible(false);  
 	     northData.setFloatable(false);  
 	     northData.setHideCollapseTool(false);  
 	     northData.setSplit(false);  
 	     northData.setMargins(new Margins(0, 0, 0, 0)); 
-	     north.setHeaderVisible(true);
+	     north.setHeaderVisible(false);
+	     north.setBodyStyleName("iptol_header");
+	     north.setBodyStyle("backgroundColor:#4B680C;");
 	   
-	     BorderLayoutData westData = new BorderLayoutData(LayoutRegion.WEST, 150);  
+	     westData = new BorderLayoutData(LayoutRegion.WEST, 150);  
 	     westData.setSplit(false);  
 	     westData.setCollapsible(true);  
 	     westData.setMargins(new Margins(5));  
 	    
-	     BorderLayoutData centerData = new BorderLayoutData(LayoutRegion.CENTER);  
+	     centerData = new BorderLayoutData(LayoutRegion.CENTER);  
 	     centerData.setMargins(new Margins(5, 0, 5, 0)); 
 	     center.setBodyStyle("padding: 25px");
 	     center.setScrollMode(Scroll.AUTOX); 
-	     center.setHeaderVisible(true);
+	     center.setHeaderVisible(false);
 	     centerData.setCollapsible(false);  
 	     centerData.setFloatable(false);  
 	     centerData.setHideCollapseTool(false);  
 	     centerData.setSplit(false);  
 	     
-	     BorderLayoutData eastData = new BorderLayoutData(LayoutRegion.EAST, 150);  
+	     eastData = new BorderLayoutData(LayoutRegion.EAST, 150);  
 	     eastData.setSplit(false);  
 	     eastData.setCollapsible(true);  
 	     eastData.setMargins(new Margins(5));  
 	   
-	     BorderLayoutData southData = new BorderLayoutData(LayoutRegion.SOUTH, 50);  
+	     southData = new BorderLayoutData(LayoutRegion.SOUTH, 50);  
 	     southData.setSplit(false);  
 	     southData.setCollapsible(false);  
 	     southData.setFloatable(false);  
 	     southData.setMargins(new Margins(0, 0, 0, 0)); 
 	     south.setHeaderVisible(false);
-	   
+	     south.setBodyStyleName("iptol_footer");
+	     
 	     add(north, northData);  
 	     add(west, westData);  
-	     add(center, centerData);  
 	     add(east, eastData);  
 	     add(south, southData);
 	     
 	     //Add basic tool bar
 	     ToolBar toolBar = new ToolBar(); 
-	     toolBar.setBorders(true);
+	     toolBar.setBorders(false);
 	        
 	     Button upload = new Button("<b>Upload</b>");
 	     
 	     //create basic menu and menu item
-	     Menu menu = new Menu();
-	     MenuItem tree_data= new MenuItem("Tree Data");
-	     tree_data.addSelectionListener(new SelectionListener<MenuEvent>() {
-			@Override
-			public void componentSelected(MenuEvent ce) {
-				Window.alert("==> tree data clicked" );
-			}
-		});
-	     menu.add(tree_data);
-	     
-	     upload.setMenu(menu);
-	     toolBar.add(upload);
+//	     Menu menu = new Menu();
+//	     MenuItem tree_data= new MenuItem("Tree Data");
+//	     tree_data.addSelectionListener(new SelectionListener<MenuEvent>() {
+//			@Override
+//			public void componentSelected(MenuEvent ce) {
+//				Window.alert("==> tree data clicked" );
+//			}
+//		});
+//	     menu.add(tree_data);
+//	     
+//	     upload.setMenu(menu);
+//	     toolBar.add(upload);
+	     toolBar.setStyleName("iptol_toolbar");
+	     toolBar.setHeight("30px");
 	     
 	     //add our logo...This should be changed to DE logo later
+	     HorizontalPanel headerPanel = new HorizontalPanel();
+	     headerPanel.addStyleName("iptol_logo");
 	     Image logo = new Image(constants.iplantLogo());
 	     logo.setHeight("125px");
-	     north.add(logo);
-	     // add to north panel
+	     headerPanel.add(logo);
+	     north.add(headerPanel);
+	     //add to north panel
 	     north.add(toolBar);
 	     
 	}
 	
+    /**
+     * 
+     * @param region a region with-in the border layout
+     * Hide a particular region from displaying
+     */
 	public void hideRegion(LayoutRegion region) {
 		layout.hide(region);
 	}
+	
+	/**
+	 * 
+	 * @param portal portal to add to the layout
+	 * @param data data for the selected layout
+	 * Add the portal to the center of layout
+	 */
+	public void addPortal(Widget portal, BorderLayoutData data) {
+		add(portal,data);
+	}
+	
 }
