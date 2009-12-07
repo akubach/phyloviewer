5a2fb1ff3427240f1db8850f435a57848620dac5
sriram srinivasan <sriram@dhcp-96870a20.ece.arizona.edu>
2009-12-07 12:32:08 -0700

    refactor code. add portal

--- src/main/java/org/iplantc/iptol/client/Controller.java
+++ src/main/java/org/iplantc/iptol/client/Controller.java
@@ -1,25 +1,28 @@
 package org.iplantc.iptol.client;
 
 import com.extjs.gxt.ui.client.Style.LayoutRegion;
-import com.extjs.gxt.ui.client.widget.Viewport;
 import com.extjs.gxt.ui.client.widget.custom.Portlet;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class Controller implements EntryPoint  {
 	//name of application 
-public static final String APPLICATION_TITLE = "Discovery Environment";
-public static final String BROWSE_TREES = "Browse Trees";
+	public static final String APPLICATION_TITLE = "Discovery Environment";
+	public static final String BROWSE_TREES = "Browse Trees";
+	
 	public void onModuleLoad() {
 		//set the app layout
 		ApplicationLayout layout = new ApplicationLayout();
 
 		//create a portal
-		DiscoveryPortal portal = new DiscoveryPortal(3);
-		//portal.setColumnWidth(0, .33);  
-		//portal.setColumnWidth(1, .33);  
-		//portal.setColumnWidth(2, .33); 
-//		
+		DiscoveryPortal portal = new DiscoveryPortal(4);
+		portal.setStyleName("iptol_portal");
+		portal.setColumnWidth(0, .25);  
+		portal.setColumnWidth(1, .25);  
+		portal.setColumnWidth(2, .25);
+		portal.setColumnWidth(3, .25); 
+		portal.setBorders(false);
+		
 		//this should be re-factored
 		TreeFilesManager treeFilesManager = new TreeFilesManager();
 		Portlet portlet = new Portlet();
@@ -28,6 +31,7 @@ public static final String BROWSE_TREES = "Browse Trees";
 		portlet.add(treeFilesManager);
 		portlet.setHeading(BROWSE_TREES);
 		portlet.setCollapsible(true);
+		portlet.setBodyStyle("font-family:'Verdana';font-size:5px;");
 		portal.add(portlet, 0);
 		layout.addPortal(portal,layout.centerData);
 		layout.north.setHeading(APPLICATION_TITLE);
