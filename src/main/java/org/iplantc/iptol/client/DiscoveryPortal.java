a9d4b45d9dda7f6b1d727da9b01908c4337bc7ce
sriram srinivasan <sriram@dhcp-96870a20.ece.arizona.edu>
2009-12-08 16:58:41 -0700

    add methods to track portlets within a portal

--- src/main/java/org/iplantc/iptol/client/DiscoveryPortal.java
+++ src/main/java/org/iplantc/iptol/client/DiscoveryPortal.java
@@ -1,6 +1,10 @@
 package org.iplantc.iptol.client;
 
+import java.util.HashMap;
+
 import com.extjs.gxt.ui.client.widget.custom.Portal;
+import com.extjs.gxt.ui.client.widget.custom.Portlet;
+
 /**
  * 
  * @author sriram
@@ -9,8 +13,19 @@ import com.extjs.gxt.ui.client.widget.custom.Portal;
  */
 public class DiscoveryPortal extends Portal{
 
+	private HashMap<String,Portlet> portlets;
+	
 	public DiscoveryPortal(int numColumns) {
 		super(numColumns);
+		portlets = new HashMap<String,Portlet>();
+	}
+	
+	public void addPortlet(Portlet p, String key) {
+		portlets.put(key, p);
+	}
+	
+	public Portlet getPortlet(String key) {
+		return portlets.get(key);
 	}
 	
 }
