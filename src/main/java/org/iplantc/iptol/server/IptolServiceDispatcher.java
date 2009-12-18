029ebcacdd83b7293366a8707cca923ee94ae4cd
sriram srinivasan <sriram@dhcp-96870a20.ece.arizona.edu>
2009-12-18 16:42:40 -0700

    parameterize service url

--- src/main/java/org/iplantc/iptol/server/IptolServiceDispatcher.java
+++ src/main/java/org/iplantc/iptol/server/IptolServiceDispatcher.java
@@ -4,7 +4,8 @@ import org.iplantc.iptol.client.IptolService;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
-public class IptolServiceDispatcher extends RemoteServiceServlet implements IptolService  {
+public class IptolServiceDispatcher extends RemoteServiceServlet implements
+		IptolService {
 
 	/**
 	 * 
@@ -15,7 +16,8 @@ public class IptolServiceDispatcher extends RemoteServiceServlet implements Ipto
 	public String getServiceData(String ServiceId) {
 		String json = null;
 		try {
-			json = ServiceHelper.call("http://localhost:14444/trees",null);
+			json = ServiceHelper.call(getServletContext().getInitParameter(
+					"iptpol.tree_retrieval_service"), null);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
