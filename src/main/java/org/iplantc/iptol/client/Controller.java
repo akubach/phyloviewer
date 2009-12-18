33b7d8fe5e86d0e8828f49194f7200df7bf6f7a3
sriram srinivasan <sriram@dhcp-96870a20.ece.arizona.edu>
2009-12-18 16:31:09 -0700

    add scroll logic for body

--- src/main/java/org/iplantc/iptol/client/Controller.java
+++ src/main/java/org/iplantc/iptol/client/Controller.java
@@ -1,43 +1,48 @@
 package org.iplantc.iptol.client;
 
 import com.extjs.gxt.ui.client.Style.LayoutRegion;
+import com.extjs.gxt.ui.client.Style.Scroll;
+import com.extjs.gxt.ui.client.fx.Draggable;
+import com.extjs.gxt.ui.client.fx.Resizable;
 import com.extjs.gxt.ui.client.widget.custom.Portlet;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
 
-public class Controller implements EntryPoint  {
-	//name of application 
+public class Controller implements EntryPoint {
+	// name of application
 	public static final String APPLICATION_TITLE = "Discovery Environment";
-	public static final String BROWSE_TREES = "Browse Trees";
-	
-	public void onModuleLoad() {
-		//set the app layout
-		ApplicationLayout layout = new ApplicationLayout();
+	public static final String BROWSE_TREES = "Data Browser";
 
-		//create a portal
-		DiscoveryPortal portal = new DiscoveryPortal(4);
+	public void onModuleLoad() {
+		// set the app layout
+		ApplicationLayout layout = new ApplicationLayout();
+		layout.assembleLayout();
+
+		// create a portal
+		WorkspacePortal portal = new WorkspacePortal(3);
 		portal.setStyleName("iptol_portal");
-		portal.setColumnWidth(0, .25);  
-		portal.setColumnWidth(1, .25);  
-		portal.setColumnWidth(2, .25);
-		portal.setColumnWidth(3, .25); 
+		portal.setColumnWidth(0, .33);
+		portal.setColumnWidth(1, .33);
+		portal.setColumnWidth(2, .33);
 		portal.setBorders(false);
 		
-		//this should be re-factored
+		// this should be re-factored
 		TreeFilesManager treeFilesManager = new TreeFilesManager();
+		treeFilesManager.assembleComponents();
 		Portlet portlet = new Portlet();
-		portlet.setHeight(400);
-		portlet.setWidth(400);
 		portlet.add(treeFilesManager);
 		portlet.setHeading(BROWSE_TREES);
 		portlet.setCollapsible(true);
-		portlet.setBodyStyle("font-family:'Verdana';font-size:5px;");
+		portlet.setAutoWidth(true);
+		portlet.setAutoHeight(true);
+		portlet.setScrollMode(Scroll.AUTO);
+		//portlet.setBodyStyle("font-family:'Verdana';font-size:5px;");
 		portal.add(portlet, 0);
-		layout.addPortal(portal,layout.centerData);
+		layout.addPortal(portal, layout.centerData);
 		layout.north.setHeading(APPLICATION_TITLE);
 		RootPanel.get().add(layout);
 		layout.hideRegion(LayoutRegion.WEST);
 		layout.hideRegion(LayoutRegion.EAST);
 	}
-	
+
 }
