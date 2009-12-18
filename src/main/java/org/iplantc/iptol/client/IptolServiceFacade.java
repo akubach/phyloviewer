5ed6e22561d7518065fa47e881d2b775c35a641b
sriram srinivasan <sriram@dhcp-96870a20.ece.arizona.edu>
2009-12-18 16:38:16 -0700

    format code

--- src/main/java/org/iplantc/iptol/client/IptolServiceFacade.java
+++ src/main/java/org/iplantc/iptol/client/IptolServiceFacade.java
@@ -5,25 +5,26 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.rpc.ServiceDefTarget;
 
 public class IptolServiceFacade {
-	
+
 	private static IptolServiceFacade iptolService;
 	private IptolServiceAsync proxy;
 	public static final String IPTOL_SERVICE = "iptolservice";
-	
+
 	private IptolServiceFacade() {
 		proxy = (IptolServiceAsync) GWT.create(IptolService.class);
-		((ServiceDefTarget) proxy).setServiceEntryPoint(GWT.getModuleBaseURL() + IPTOL_SERVICE);
+		((ServiceDefTarget) proxy).setServiceEntryPoint(GWT.getModuleBaseURL()
+				+ IPTOL_SERVICE);
 	}
-	
+
 	public static IptolServiceFacade getInstance() {
-		if(iptolService == null) {
+		if (iptolService == null) {
 			iptolService = new IptolServiceFacade();
 		}
 		return iptolService;
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public void getServiceData(String serviceId,AsyncCallback callback) {
+	public void getServiceData(String serviceId, AsyncCallback callback) {
 		proxy.getServiceData(serviceId, callback);
 	}
 }
