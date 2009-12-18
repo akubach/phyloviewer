07cd12696057bbc3a39f64d21a4e29a03a06e171
sriram srinivasan <sriram@dhcp-96870a20.ece.arizona.edu>
2009-12-18 16:44:03 -0700

    refacor code.fix for null pointer exception

--- src/main/java/org/iplantc/iptol/server/ServiceHelper.java
+++ src/main/java/org/iplantc/iptol/server/ServiceHelper.java
@@ -10,52 +10,57 @@ import java.util.Map;
 
 public class ServiceHelper {
 
-	public static String buildWebQuery(Map<String,String> params) throws Exception {
+	public static String buildWebQuery(Map<String, String> params)
+			throws Exception {
+
 		StringBuilder sb = new StringBuilder();
-	   for(Map.Entry<String,String> entry : params.entrySet()) {		
-		 	         
-			String key = URLEncoder.encode(entry.getKey(),"UTF-8");
-		 	String value = URLEncoder.encode(entry.getValue(),"UTF-8");
-		 	
-		 	sb.append(key).append("=").append(value).append("&");
+		for (Map.Entry<String, String> entry : params.entrySet()) {
+			String key = URLEncoder.encode(entry.getKey(), "UTF-8");
+			String value = URLEncoder.encode(entry.getValue(), "UTF-8");
+			sb.append(key).append("=").append(value).append("&");
 		}
-		
-		return sb.toString().substring(0,sb.length() - 1);
+
+		return sb.toString().substring(0, sb.length() - 1);
 
 	}
-	
-	 public static String call(String address,Map<String,String> params) throws Exception    {       
-	 
-	    	String ret = "";
-	    	String query = null;
-	    	if(params!=null) {
-	    		query = buildWebQuery(params);
-	    	}
-	        URL url = new URL(address);
-	 
-	        // make post mode connection
-	        URLConnection urlc = url.openConnection();
-	        
-	        urlc.setDoOutput(true);
-	 
-	        // send query
-	        PrintStream ps = new PrintStream(urlc.getOutputStream());
-	        ps.print(query);
-	        ps.close();
-	 
-	        // retrieve result
-	        BufferedReader br = new BufferedReader(new InputStreamReader(urlc.getInputStream(),"UTF-8"));
-	        StringBuilder sb = new StringBuilder();
-	        String line;
-	        while ((line = br.readLine()) != null) 
-	        {
-	            sb.append(line);
-	            sb.append("\n");
-	        }
-	        br.close();
-	        ret = sb.toString();
-	 
-	        return ret;
-	    }
+
+	public static String call(String address, Map<String, String> params)
+			throws Exception {
+
+		String ret = "";
+		String query = null;
+
+		if (params != null) {
+			query = buildWebQuery(params);
+		}
+
+		URL url = new URL(address);
+
+		// make post mode connection
+		URLConnection urlc = url.openConnection();
+
+		urlc.setDoOutput(true);
+
+		// send query
+		PrintStream ps = new PrintStream(urlc.getOutputStream());
+		ps.print(query);
+		ps.close();
+
+		// retrieve result
+		BufferedReader br = new BufferedReader(new InputStreamReader(urlc
+				.getInputStream(), "UTF-8"));
+		StringBuilder sb = new StringBuilder();
+		String line;
+
+		while ((line = br.readLine()) != null) {
+			sb.append(line);
+			sb.append("\n");
+		}
+
+		br.close();
+		ret = sb.toString();
+
+		return ret;
+	}
 
 }
