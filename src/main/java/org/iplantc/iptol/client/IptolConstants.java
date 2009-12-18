0af9bd8776abee89fb6ae890ad42cb654499dfbb
sriram srinivasan <sriram@dhcp-96870a20.ece.arizona.edu>
2009-12-18 16:36:13 -0700

    format code

--- src/main/java/org/iplantc/iptol/client/IptolConstants.java
+++ src/main/java/org/iplantc/iptol/client/IptolConstants.java
@@ -5,9 +5,14 @@ import com.google.gwt.i18n.client.Constants;
 public interface IptolConstants extends Constants {
 
 	int statusInterval();
+
 	String noFiles();
+
 	String fileUploadSuccess();
+
 	String fileUploadFailed();
+
 	String iplantLogo();
+
 	String treeFilesListService();
 }
