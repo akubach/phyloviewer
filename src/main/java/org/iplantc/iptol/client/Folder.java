25e9baba0cb4c069bafb69db5299e8bfd7733232
sriram srinivasan <sriram@dhcp-96870a20.ece.arizona.edu>
2009-12-30 12:05:22 -0700

    represent a folder in the Data Browser

---
+++ src/main/java/org/iplantc/iptol/client/Folder.java
@@ -0,0 +1,41 @@
+package org.iplantc.iptol.client;
+
+import com.extjs.gxt.ui.client.data.BaseTreeModel;
+
+import java.io.Serializable;
+
+public class Folder extends BaseTreeModel implements Serializable {
+	 /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private static int ID = 0;
+	  
+	  public Folder() {
+	    set("id", ID++);
+	  }
+
+	  public Folder(String name) {
+	    set("id", ID++);
+	    set("name", name);
+	  }
+
+	  public Folder(String name, BaseTreeModel[] children) {
+	    this(name);
+	    for (int i = 0; i < children.length; i++) {
+	      add(children[i]);
+	    }
+	  }
+
+	  public Integer getId() {
+	    return (Integer) get("id");
+	  }
+
+	  public String getName() {
+	    return (String) get("name");
+	  }
+
+	  public String toString() {
+	    return getName();
+	  }
+}
