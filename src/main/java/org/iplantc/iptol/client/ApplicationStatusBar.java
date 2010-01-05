ed24b4335e99738b1670b0b32943ccb1d82deb6c
sriram srinivasan <sriram@dhcp-96870a20.ece.arizona.edu>
2010-01-05 11:12:39 -0700

    application status bar widget

---
+++ src/main/java/org/iplantc/iptol/client/ApplicationStatusBar.java
@@ -0,0 +1,16 @@
+package org.iplantc.iptol.client;
+
+import com.extjs.gxt.ui.client.widget.toolbar.ToolBar;
+/**
+ * 
+ * @author sriram
+ * A class that represents the application status bar. 
+ * Application components can add/remove status. 
+ */
+public class ApplicationStatusBar extends ToolBar {
+
+	public ApplicationStatusBar() {
+		super();
+	}
+
+}
