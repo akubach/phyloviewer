4cb0e17e396630e7c99402c9e193ec61c1989503
sriram srinivasan <sriram@dhcp-96870a20.ece.arizona.edu>
2010-01-06 16:59:44 -0700

    refactor code to integrate with andy history and event management

--- src/main/java/org/iplantc/iptol/client/Controller.java
+++ src/main/java/org/iplantc/iptol/client/Controller.java
@@ -6,46 +6,31 @@ import com.extjs.gxt.ui.client.fx.Draggable;
 import com.extjs.gxt.ui.client.fx.Resizable;
 import com.extjs.gxt.ui.client.widget.custom.Portlet;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class Controller implements EntryPoint {
 	// name of application
 	public static final String APPLICATION_TITLE = "Discovery Environment";
-	public static final String BROWSE_TREES = "Data Browser";
 
 	public void onModuleLoad() {
-		// set the app layout
-		ApplicationLayout layout = new ApplicationLayout();
-		layout.assembleLayout();
-
-		// create a portal
-//		WorkspacePortal portal = new WorkspacePortal(3);
-//		portal.setStyleName("iptol_portal");
-//		portal.setColumnWidth(0, .33);
-//		portal.setColumnWidth(1, .33);
-//		portal.setColumnWidth(2, .33);
-//		portal.setBorders(false);
-//		
-//		// this should be re-factored
-//		TreeFilesManager treeFilesManager = new TreeFilesManager();
-//		treeFilesManager.assembleComponents();
-//		Portlet portlet = new Portlet();
-//		portlet.add(treeFilesManager);
-//		portlet.setHeading(BROWSE_TREES);
-//		portlet.setCollapsible(true);
-//		portlet.setAutoWidth(true);
-//		portlet.setAutoHeight(true);
-//		portlet.setScrollMode(Scroll.AUTO);
-//		//portlet.setBodyStyle("font-family:'Verdana';font-size:5px;");
-//		portal.add(portlet, 0);
-//		layout.addPortal(portal, layout.centerData);
-//		layout.north.setHeading(APPLICATION_TITLE);
 		
-		DataBrowserTree dataManagerTree = new DataBrowserTree();
-		dataManagerTree.assembleView();
-		layout.add(dataManagerTree, layout.westData);
-		RootPanel.get().add(layout);
-		layout.hideRegion(LayoutRegion.EAST);
+		PresentationManager mgr = new PresentationManager();
+		String token = History.getToken();
+		
+		if(!mgr.handleToken(token,false))
+		{
+			mgr.handleToken("workspace");
+		}	
+		
+		// set the app layout
+//		ApplicationLayout layout = new ApplicationLayout();
+//		layout.assembleLayout();
+//		DataBrowserTree dataManagerTree = new DataBrowserTree();
+//		dataManagerTree.assembleView();
+//		layout.add(dataManagerTree, layout.westData);
+//		RootPanel.get().add(layout);
+//		layout.hideRegion(LayoutRegion.EAST);
 	}
 
 }
