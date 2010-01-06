f4684f6bcc9bcb6eba04ff1a24c1741b4dac73af
sriram srinivasan <sriram@dhcp-96870a20.ece.arizona.edu>
2010-01-06 16:58:55 -0700

    refactor code for handling events

--- src/main/java/org/iplantc/iptol/client/ApplicationStatusBar.java
+++ src/main/java/org/iplantc/iptol/client/ApplicationStatusBar.java
@@ -1,6 +1,12 @@
 package org.iplantc.iptol.client;
 
+import org.iplantc.iptol.client.events.DataBrowserNodeClickEvent;
+import org.iplantc.iptol.client.events.DataBrowserNodeClickEventHandler;
+
+import com.extjs.gxt.ui.client.widget.Status;
 import com.extjs.gxt.ui.client.widget.toolbar.ToolBar;
+import com.google.gwt.event.shared.HandlerManager;
+import com.google.gwt.user.client.Window;
 /**
  * 
  * @author sriram
@@ -9,8 +15,39 @@ import com.extjs.gxt.ui.client.widget.toolbar.ToolBar;
  */
 public class ApplicationStatusBar extends ToolBar {
 
-	public ApplicationStatusBar() {
+	private HandlerManager eventbus;
+	private Status status_left;
+	private Status status_center;
+	private Status status_right;
+	
+	public ApplicationStatusBar(HandlerManager eventbus) {
 		super();
+		this.eventbus = eventbus;
+		status_left = new Status();
+		status_center = new Status();
+		status_right = new Status();
+		assembleStatusBars();
+		eventbus.addHandler(DataBrowserNodeClickEvent.TYPE, new DataBrowserNodeClickEventHandler() {
+			@Override
+			public void onNodeClick(DataBrowserNodeClickEvent dbnce) {
+				File file = dbnce.getFile();
+				if(file instanceof Folder) {
+					status_left.setText(file.getChildren().size() + " file(s)");
+				} else {
+					status_left.setText(dbnce.getFile().getInfo().getLabel() + "-" + dbnce.getFile().getInfo().getUploaded());
+				}
+			}
+		});
 	}
 
+	
+	private void assembleStatusBars() {
+		this.add(status_left);
+		this.add(status_center);
+		this.add(status_right);
+	}
+	
+	
+	
+	
 }
