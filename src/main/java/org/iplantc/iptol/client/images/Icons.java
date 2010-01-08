eb1ed7776c4d46ba77e65d74caf32bb9b7358919
sriram srinivasan <sriram@dhcp-96870a20.ece.arizona.edu>
2010-01-07 17:04:45 -0700

    imagebudle and classes to access it

---
+++ src/main/java/org/iplantc/iptol/client/images/Icons.java
@@ -0,0 +1,21 @@
+package org.iplantc.iptol.client.images;
+
+import com.google.gwt.user.client.ui.AbstractImagePrototype;
+import com.google.gwt.user.client.ui.ImageBundle;
+
+public interface Icons extends ImageBundle{
+	
+	@Resource("Green.png")
+	AbstractImagePrototype green();
+	
+	@Resource("Orange.png")
+	AbstractImagePrototype orange();
+	
+	
+	@Resource("Red.png")
+	AbstractImagePrototype red();
+	
+	
+	@Resource("list-items.gif")
+	AbstractImagePrototype listItems();
+}
