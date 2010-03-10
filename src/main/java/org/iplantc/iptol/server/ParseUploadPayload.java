af65f0d8666ae1c807f66d9584e7b76bc8b38745
Donald Barre <dbarre@iplantcollaborative.org>
2010-03-10 12:51:40 -0700

    IPTOL-132 : remove debug statements and clean up code

--- src/main/java/org/iplantc/iptol/server/ParseUploadPayload.java
+++ src/main/java/org/iplantc/iptol/server/ParseUploadPayload.java
@@ -1,22 +1,23 @@
 package org.iplantc.iptol.server;
 
-import org.apache.log4j.Logger;
+import java.util.Set;
+
+import org.apache.commons.lang.StringUtils;
 import org.mule.api.MuleMessage;
 import org.mule.api.transformer.TransformerException;
 import org.mule.config.i18n.MessageFactory;
 import org.mule.transformer.AbstractMessageAwareTransformer;
 
 /**
- * When a file is uploaded, the HTTP body, i.e. payload, contains
+ * When a file is uploaded, the Mule message contains the Content-Type and the
+ * payload is the multipart containing the file contents.  Use the HTTP MultiPartParser
+ * to extract the file contents.
  * @author Donald A. Barre
  */
 public class ParseUploadPayload extends AbstractMessageAwareTransformer {
 
 	private static final String WORKSPACE_ID = "workspaceId";
 	private static final String FOLDER_ID = "folderId";
-	private static final String INVALID_ID = null;
-
-	private static final Logger LOG = Logger.getLogger(ParseUploadPayload.class);
 
 	/* (non-Javadoc)
 	 * @see org.mule.transformer.AbstractMessageAwareTransformer#transform(org.mule.api.MuleMessage, java.lang.String)
@@ -29,25 +30,20 @@ public class ParseUploadPayload extends AbstractMessageAwareTransformer {
 			throw new TransformerException(MessageFactory.createStaticMessage("Upload Payload is not a string"));
 		}
 
-		String contentType = (String) message.getProperty("Content-Type");
+		String contentType = getContentType(message);
 		if (contentType == null) {
 			throw new TransformerException(MessageFactory.createStaticMessage("No content-type in multi-part upload"));
 		}
 
-		LOG.debug("Content-Type is " + contentType);
-		LOG.debug((String) message.getPayload());
-
 		MultiPartParser parser = new MultiPartParser(contentType, (String) message.getPayload());
 		FilePart filePart = parser.readNextPart();
-		LOG.debug("***** FILE CONTENTS ***");
-		LOG.debug(filePart.getContents());
 
 		/*
 		 * The folder is optional.  If not specified, the default upload folder is used.
 		 */
 		Long folderId = null;
 		if (message.getProperty(FOLDER_ID) != null) {
-			folderId = Long.valueOf(message.getStringProperty(FOLDER_ID, INVALID_ID));
+			folderId = getLongValue(message, FOLDER_ID);
 		}
 
 		/*
@@ -56,11 +52,42 @@ public class ParseUploadPayload extends AbstractMessageAwareTransformer {
 		Object newPayload[] = new Object[folderId == null ? 3 : 4];
 		newPayload[0] = filePart.getContents();
 		newPayload[1] = filePart.getFilename();
-		newPayload[2] = Long.valueOf(message.getStringProperty(WORKSPACE_ID, INVALID_ID));
+		newPayload[2] = getLongValue(message, WORKSPACE_ID);
 		if (folderId != null) {
 			newPayload[3] = folderId;
 		}
 
 		return newPayload;
+	}
+
+	/**
+	 * Get a Long property value.
+	 * @param message
+	 * @param name the name of the property
+	 * @return the value or null if property name is not found
+	 */
+	private Long getLongValue(MuleMessage message, String name) {
+		String value = message.getStringProperty(FOLDER_ID, null);
+		if (value == null) {
+			return null;
+		}
+		return Long.valueOf(value);
+	}
+
+	/**
+	 * Get the Content-Type from the message.  Handle the case where
+	 * the property name can be any case.
+	 * @param message
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	private String getContentType(MuleMessage message) {
+		Set<String> names = message.getPropertyNames();
+		for (String name : names) {
+			if (StringUtils.equalsIgnoreCase(name, "content-type")) {
+				return (String) message.getProperty(name);
+			}
+		}
+		return null;
 	}
 }
