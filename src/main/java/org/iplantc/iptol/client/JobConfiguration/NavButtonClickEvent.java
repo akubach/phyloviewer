099f88336cac673d2e11f8dd823600babbb85e11
sriram srinivasan <sriram@dhcp-96870a20.ece.arizona.edu>
2010-03-15 10:12:40 -0700

    event classes for enabling left side nav. panel

---
+++ src/main/java/org/iplantc/iptol/client/JobConfiguration/NavButtonClickEvent.java
@@ -0,0 +1,35 @@
+package org.iplantc.iptol.client.JobConfiguration;
+
+import com.google.gwt.event.shared.GwtEvent;
+
+public class NavButtonClickEvent extends GwtEvent<NavButtonEventClickEventHandler> {
+
+public static final GwtEvent.Type<NavButtonEventClickEventHandler> TYPE = new GwtEvent.Type<NavButtonEventClickEventHandler>();
+	
+	private JobStep step;
+	
+	public void setStep(JobStep step) {
+		this.step = step;
+	}
+
+	public JobStep getStep() {
+		return step;
+	}
+	
+	public NavButtonClickEvent(JobStep step) {
+		this.setStep(step);
+	}
+	
+	
+	@Override
+	protected void dispatch(NavButtonEventClickEventHandler handler) {
+		handler.onClick(this);
+	}
+
+	@Override
+	public Type<NavButtonEventClickEventHandler> getAssociatedType() {
+		return TYPE;
+	}
+
+
+}
