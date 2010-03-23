94193f4e6d92d641cefdb8e831d2bb6b7a7e0824
sriram srinivasan <sriram@dhcp-96870a20.ece.arizona.edu>
2010-03-23 16:40:53 -0700

    service facade for Tree realted services

--- src/main/java/org/iplantc/iptol/client/JobConfiguration/JobToolBarPrevClickEventHandler.java
+++
@@ -1,8 +0,0 @@
-package org.iplantc.iptol.client.JobConfiguration;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface JobToolBarPrevClickEventHandler extends EventHandler {
-
-	void onPrevClick(JobToolBarPrevClickEvent prev);
-}
