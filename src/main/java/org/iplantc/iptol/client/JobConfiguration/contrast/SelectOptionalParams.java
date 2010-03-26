92d81e137b0105edb73064e61c6e1132332f945b
sriram srinivasan <sriram@dhcp-96870a20.ece.arizona.edu>
2010-03-26 10:19:08 -0700

    remove unwanted imports

--- src/main/java/org/iplantc/iptol/client/JobConfiguration/contrast/SelectOptionalParams.java
+++ src/main/java/org/iplantc/iptol/client/JobConfiguration/contrast/SelectOptionalParams.java
@@ -7,17 +7,14 @@ import org.iplantc.iptol.client.JobConfiguration.Card;
 import org.iplantc.iptol.client.JobConfiguration.DataSelectedEvent;
 
 import com.extjs.gxt.ui.client.event.BaseEvent;
-import com.extjs.gxt.ui.client.event.EventType;
 import com.extjs.gxt.ui.client.event.Events;
 import com.extjs.gxt.ui.client.event.Listener;
-import com.extjs.gxt.ui.client.widget.VerticalPanel;
 import com.extjs.gxt.ui.client.widget.form.CheckBox;
 import com.extjs.gxt.ui.client.widget.form.FormPanel;
 import com.extjs.gxt.ui.client.widget.layout.FormData;
 import com.extjs.gxt.ui.client.widget.layout.VBoxLayout;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.shared.HandlerManager;
-import com.google.gwt.user.client.Window;
 
 /**
  * @author sriram Provides UI to select params for the independent contrast job
