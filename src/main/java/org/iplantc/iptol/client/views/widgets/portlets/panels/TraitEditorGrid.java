22410eb4ff971f901a572356ac76de8217e3d9a9
sriram srinivasan <sriram@dhcp-96870a20.ece.arizona.edu>
2010-03-27 13:54:45 -0700

    add logic to save back traits to de

--- src/main/java/org/iplantc/iptol/client/views/widgets/portlets/panels/TraitEditorGrid.java
+++ src/main/java/org/iplantc/iptol/client/views/widgets/portlets/panels/TraitEditorGrid.java
@@ -3,13 +3,17 @@ package org.iplantc.iptol.client.views.widgets.portlets.panels;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.iplantc.iptol.client.IptolDisplayStrings;
+
 import com.extjs.gxt.ui.client.Style.Scroll;
+import com.extjs.gxt.ui.client.data.BaseListLoader;
 import com.extjs.gxt.ui.client.data.BasePagingLoader;
-import com.extjs.gxt.ui.client.data.JsonReader;
+import com.extjs.gxt.ui.client.data.JsonLoadResultReader;
+import com.extjs.gxt.ui.client.data.ListLoadResult;
+import com.extjs.gxt.ui.client.data.MemoryProxy;
 import com.extjs.gxt.ui.client.data.ModelData;
 import com.extjs.gxt.ui.client.data.ModelType;
 import com.extjs.gxt.ui.client.data.PagingLoadResult;
-import com.extjs.gxt.ui.client.data.PagingModelMemoryProxy;
 import com.extjs.gxt.ui.client.event.ButtonEvent;
 import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.store.ListStore;
@@ -24,9 +28,11 @@ import com.extjs.gxt.ui.client.widget.grid.EditorGrid;
 import com.extjs.gxt.ui.client.widget.layout.FitLayout;
 import com.extjs.gxt.ui.client.widget.toolbar.PagingToolBar;
 import com.extjs.gxt.ui.client.widget.toolbar.ToolBar;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONValue;
+import com.google.gwt.user.client.Window;
 
 public class TraitEditorGrid {
 
@@ -38,12 +44,15 @@ public class TraitEditorGrid {
 
 	private ModelType type;
 
-	private JsonReader<ModelData> reader;
+	private JsonLoadResultReader<ListLoadResult<ModelData>> reader;
 	private ArrayList<ModelData> model = null;
 	private PagingToolBar pagingToolBar = null;
 	private BasePagingLoader<PagingLoadResult<ModelData>> loader = null;
+	
+	private IptolDisplayStrings displayStrings = (IptolDisplayStrings) GWT
+	.create(IptolDisplayStrings.class);
 
-	public TraitEditorGrid(String json) {	
+	public TraitEditorGrid(String json) {
 		parser = new TraitDataJsonParser(json);
 		columns = new ArrayList<ColumnConfig>();
 	}
@@ -51,77 +60,77 @@ public class TraitEditorGrid {
 	public void parseJson() {
 		parser.parseRoot();
 	}
-	
+
 	public ContentPanel assembleView() {
 		loadData();
 		panel = new ContentPanel();
-	
+
 		panel.setScrollMode(Scroll.NONE);
 		panel.setHeaderVisible(true);
 		panel.setHeaderVisible(false);
 		panel.setLayout(new FitLayout());
-		
+
 		panel.add(grid);
 		panel.setTopComponent(buildToolBar());
 		panel.setBottomComponent(pagingToolBar);
-		
+
 		return panel;
 	}
-	
+
 	private ToolBar buildToolBar() {
 		ToolBar toolBar = new ToolBar();
 
-		Button add = new Button("Add");
-		toolBar.add(add);
-		add.addSelectionListener(new SelectionListener<ButtonEvent>() {
-			@SuppressWarnings("unchecked")
-			@Override
-			public void componentSelected(ButtonEvent ce) {
-				// create a mock row and add
-//				ArrayList<ModelData> data = (ArrayList<ModelData>) reader.read(
-//						null, (TraitDataJsonParser
-//								.getMockUp(parser.getHeader())).toString());
-//				
-//				grid.stopEditing(false);
-//				store.insert((ModelData)data.get(0), 0);
-//				grid.startEditing(0,0);
-			}
-		});
-		
-		Button save = new Button("Save");
+		// Button add = new Button("Add");
+		// toolBar.add(add);
+		// add.addSelectionListener(new SelectionListener<ButtonEvent>() {
+		// @SuppressWarnings("unchecked")
+		// @Override
+		// public void componentSelected(ButtonEvent ce) {
+		// // create a mock row and add
+		// // ArrayList<ModelData> data = (ArrayList<ModelData>) reader.read(
+		// // null, (TraitDataJsonParser
+		// // .getMockUp(parser.getHeader())).toString());
+		// //
+		// // grid.stopEditing(false);
+		// // store.insert((ModelData)data.get(0), 0);
+		// // grid.startEditing(0,0);
+		// }
+		// });
+
+		Button save = new Button(displayStrings.save());
 		toolBar.add(save);
 		save.addSelectionListener(new SelectionListener<ButtonEvent>() {
 			@Override
 			public void componentSelected(ButtonEvent ce) {
 				store.commitChanges();
-				
+				TraitDataJsonGen gen = new TraitDataJsonGen(store.getModels(), parser.getHeader().isArray());
+				gen.generateJson();
 			}
-			
+
 		});
-		
-		Button reset = new Button("Reset");
+
+		Button reset = new Button(displayStrings.reset());
 		toolBar.add(reset);
 		reset.addSelectionListener(new SelectionListener<ButtonEvent>() {
 			@Override
 			public void componentSelected(ButtonEvent ce) {
 				store.rejectChanges();
 			}
-			
-			
+
 		});
-		
-		Button delete = new Button("Delete");
-		toolBar.add(delete);
-		delete.addSelectionListener(new SelectionListener<ButtonEvent>() {
-			@Override
-			public void componentSelected(ButtonEvent ce) {
-				for (ModelData data : grid.getSelectionModel().getSelectedItems()) {
-					store.remove(data);
-				}
-			}
-			
-			
-		});
+
+		// Button delete = new Button("Delete");
+		// toolBar.add(delete);
+		// delete.addSelectionListener(new SelectionListener<ButtonEvent>() {
+		// @Override
+		// public void componentSelected(ButtonEvent ce) {
+		// for (ModelData data : grid.getSelectionModel().getSelectedItems()) {
+		// store.remove(data);
+		// }
+		// }
+		//			
+		//			
+		// });
 		return toolBar;
 	}
 
@@ -133,53 +142,71 @@ public class TraitEditorGrid {
 		// create modeltype
 		type = new ModelType();
 		type.setRoot("data");
+		type.addField("id", "id");
 
-		CheckBoxSelectionModel<ModelData> checkbox = new CheckBoxSelectionModel<ModelData>(); 
+		CheckBoxSelectionModel<ModelData> checkbox = new CheckBoxSelectionModel<ModelData>();
 		columns.add(checkbox.getColumn());
-		
+
 		TextField<String> text = new TextField<String>();
 		text.setAllowBlank(false);
 		CellEditor editor = new CellEditor(text);
-		
-      	for (int i = 0; i < headers.size(); i++) {
-      		JSONValue val = headers.get(i);
+
+		for (int i = 0; i < headers.size(); i++) {
+			JSONValue val = headers.get(i);
 			JSONObject obj = val.isObject();
-    		column = new ColumnConfig(TraitDataJsonParser.trim(obj.get("label").toString()),TraitDataJsonParser.trim(obj.get("label").toString()), 100);
-			column.setEditor(editor);
+			column = new ColumnConfig(TraitDataJsonParser.trim(obj.get("id")
+					.toString()), TraitDataJsonParser.trim(obj.get("label")
+					.toString()), 100);
+
+			// cannot edit species name for now
+			if (i != 0) {
+				column.setEditor(editor);
+			}
+
 			columns.add(column);
-			type.addField(TraitDataJsonParser.trim(TraitDataJsonParser.trim(obj.get("label").toString())));
+			type.addField(TraitDataJsonParser.trim(TraitDataJsonParser.trim(obj
+					.get("id").toString())),
+					TraitDataJsonParser.trim(TraitDataJsonParser.trim(obj.get(
+							"label").toString())));
 			column = null;
 		}
 
 		// create the column model
 		ColumnModel cm = new ColumnModel(columns);
 
-		reader = new JsonReader<ModelData>(type);
+		reader = new JsonLoadResultReader<ListLoadResult<ModelData>>(type);  
 
-		if (reader != null) {
-			model = (ArrayList<ModelData>) reader.read(null, parser.getData()
-					.toString());
-		}
+//		if (reader != null) {
+//			model = (ArrayList<ModelData>) reader.read(null, parser.getData()
+//					.toString());
+//		}
 
 		// add paging support for a local collection of models
-		PagingModelMemoryProxy proxy = new PagingModelMemoryProxy(model);
-		//			
-		// //set loader for loading json data into grid
-		loader = new BasePagingLoader<PagingLoadResult<ModelData>>(proxy);
-		loader.setRemoteSort(true);
-
+//		PagingModelMemoryProxy proxy = new PagingModelMemoryProxy(model);
+//		//			
+//		// //set loader for loading json data into grid
+//		loader = new BasePagingLoader<PagingLoadResult<ModelData>>(proxy);
+//		loader.setRemoteSort(true);
+//
+//		store = new ListStore<ModelData>(loader);
+//		grid = new EditorGrid<ModelData>(store, cm);
+//
+//		grid.addPlugin(checkbox);
+//		grid.setSelectionModel(checkbox);
+//		grid.setBorders(true);
+//
+//		pagingToolBar = new PagingToolBar(10);
+//
+//		pagingToolBar.bind(loader);
+//
+//		loader.load(0, 10);
+		
+		MemoryProxy<ModelData> proxy = new MemoryProxy<ModelData>(reader.read(null, parser.getData().toString()));
+		final BaseListLoader<ListLoadResult<ModelData>> loader = new BaseListLoader<ListLoadResult<ModelData>>(proxy);
 		store = new ListStore<ModelData>(loader);
 		grid = new EditorGrid<ModelData>(store, cm);
+		loader.load();
 		
-		grid.addPlugin(checkbox);
-		grid.setSelectionModel(checkbox);
-		grid.setBorders(true);
-
-		pagingToolBar = new PagingToolBar(10);
-	
-		pagingToolBar.bind(loader);
-
-		loader.load(0, 10);
 	}
 
 }
