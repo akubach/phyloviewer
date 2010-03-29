44e6b73dc734e1120d8f39eb86aced102e2b0fe7
Evan Deaubl <evan@iplantcollaborative.org>
2010-03-29 12:33:01 -0700

    ForceFileExtension transformer was not running, causing files to display in the browser, rather than saving

--- src/main/java/org/iplantc/iptol/server/ForceFileExtension.java
+++ src/main/java/org/iplantc/iptol/server/ForceFileExtension.java
@@ -1,6 +1,5 @@
 package org.iplantc.iptol.server;
 
-import org.iplantc.phyloparser.model.FileData;
 import org.iplantc.treedata.model.File;
 import org.mule.api.transformer.TransformerException;
 import org.mule.transformer.AbstractTransformer;
@@ -9,14 +8,17 @@ public class ForceFileExtension extends AbstractTransformer {
 	private String extension;
 	
 	public ForceFileExtension() {
-		registerSourceType(File.class);
-		setReturnClass(File.class);
 	}
 	
 	@Override
 	protected Object doTransform(Object obj, String encoding)
 			throws TransformerException {
-		File file = (File)obj;
+		File file;
+		if (obj instanceof Object[]) {
+			file = (File)((Object[])obj)[0];
+		} else {
+			file = (File)obj;
+		}
 		if (!file.getName().toLowerCase().endsWith(extension)) {
 			file.setName(file.getName() + extension);
 		}
