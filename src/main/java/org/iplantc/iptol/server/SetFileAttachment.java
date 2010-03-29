784664fa62bcdf958b1ff929384ccb2fe11d4146
Donald Barre <dbarre@iplantcollaborative.org>
2010-03-29 16:28:19 -0700

    IPTOL-232 : Set the content-disposition so that the browser will prompt the user on a download of a file

---
+++ src/main/java/org/iplantc/iptol/server/SetFileAttachment.java
@@ -0,0 +1,29 @@
+package org.iplantc.iptol.server;
+
+import org.iplantc.treedata.model.File;
+import org.mule.api.MuleMessage;
+import org.mule.api.transformer.TransformerException;
+import org.mule.transformer.AbstractMessageAwareTransformer;
+
+/**
+ * Sets the content disposition in the HTTP Response Header so that the browser
+ * will ask the user where they want to save the file.
+ * @author Donald A. Barre
+ */
+public class SetFileAttachment extends AbstractMessageAwareTransformer {
+
+	/* (non-Javadoc)
+	 * @see org.mule.transformer.AbstractMessageAwareTransformer#transform(org.mule.api.MuleMessage, java.lang.String)
+	 */
+	@Override
+	public Object transform(MuleMessage message, String encoding)
+			throws TransformerException {
+
+		File file = (File) message.getPayload();
+		if (file != null) {
+			message.setProperty("Content-diposition", "attachment, filename=" + file.getName());
+		}
+
+        return message.getPayload();
+	}
+}
