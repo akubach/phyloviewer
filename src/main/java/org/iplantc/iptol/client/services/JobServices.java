706a62af0aaa6b99f89cd04413650246ae9d5a8a
sriram srinivasan <sriram@dhcp-96870a20.ece.arizona.edu>
2010-03-30 16:22:49 -0700

    add service calls for job save run and delete

--- src/main/java/org/iplantc/iptol/client/services/JobServices.java
+++ src/main/java/org/iplantc/iptol/client/services/JobServices.java
@@ -7,13 +7,28 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public class JobServices {
 
-	public static void saveJob(String jsonParams,AsyncCallback<String> callback) {
+	public static void saveContrastJob(String jsonParams,AsyncCallback<String> callback) {
 		ServiceCallWrapper wrapper = new ServiceCallWrapper(ServiceCallWrapper.Type.POST,"http://" + Window.Location.getHostName() + ":14444/contrast",jsonParams);
 		IptolServiceFacade.getInstance().getServiceData(wrapper,callback);
 	}
 	
-	public static void runJob(String jobid, AsyncCallback<String> callback) {
+	public static void runContrastJob(String jobid, AsyncCallback<String> callback) {
 		ServiceCallWrapper wrapper = new ServiceCallWrapper(ServiceCallWrapper.Type.POST,"http://" + Window.Location.getHostName() + ":14444/contrast/"+ jobid + "/run",jobid);
+		IptolServiceFacade.getInstance().getServiceData(wrapper,callback);
+	}
+	
+	public static void getContrastJobs(AsyncCallback<String> callback) {
+		ServiceCallWrapper wrapper = new ServiceCallWrapper(ServiceCallWrapper.Type.GET,"http://" + Window.Location.getHostName() + ":14444/contrast");
+		IptolServiceFacade.getInstance().getServiceData(wrapper,callback);
+	}
+	
+	public static void getContrastJobConfig(String jobid, AsyncCallback<String> callback) {
+		ServiceCallWrapper wrapper = new ServiceCallWrapper(ServiceCallWrapper.Type.GET,"http://" + Window.Location.getHostName() + ":14444/contrast/" + jobid);
+		IptolServiceFacade.getInstance().getServiceData(wrapper,callback);
+	}
+	
+	public static void deleteContrastJob(String jobid,AsyncCallback<String> callback) {
+		ServiceCallWrapper wrapper = new ServiceCallWrapper(ServiceCallWrapper.Type.DELETE,"http://" + Window.Location.getHostName() + ":14444/contrast/" + jobid);
 		IptolServiceFacade.getInstance().getServiceData(wrapper,callback);
 	}
 	
