c29fd60fad7a1c97c3baa4613bf3ddfbfc5089d3
Dennis Roberts <dennis@iplantcollaborative.org>
2010-04-20 18:12:04 -0700

    Changed the way that the discovery environment is secured in order to correct several authentication problems.

---
+++ src/main/java/org/iplantc/iptol/client/AsyncCallbackWrapper.java
@@ -0,0 +1,62 @@
+package org.iplantc.iptol.client;
+
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.StatusCodeException;
+
+/**
+ * Detects when the user is not logged in to the application and redirects the user to the login page.  The
+ * authentication filter always responds with a 403 status code, but we have also have to check for a status
+ * code of zero because that seems to be what StatusCodeException.getStatusCode() always returns.
+ *
+ * @author Dennis Roberts
+ *
+ * @param <T> the type of the result we're expecting to get from the server.
+ */
+public class AsyncCallbackWrapper<T> implements AsyncCallback<T> {
+
+    /**
+     * The callback that we're wrapping.
+     */
+    AsyncCallback<T> callback;
+
+    /**
+     * Creates a new callback wrapper.
+     *
+     * @param callback the callback that we're wrapping.
+     */
+    public AsyncCallbackWrapper(AsyncCallback<T> callback) {
+        this.callback = callback;
+    }
+
+    /**
+     * Called whenever a call to the server fails.  If the call failed because of an HTTP status code and that status
+     * code is either 403 or wasn't recorded then we assume that the user isn't logged in and redirect the user to the
+     * login page.  The callback that we're wrapping deals with all other errors.
+     * 
+     * @param error the exception or error that indicates why the call failed.
+     */
+    @Override
+    public void onFailure(Throwable error) {
+        if (error instanceof StatusCodeException) {
+            int statusCode = ((StatusCodeException) error).getStatusCode();
+            if (statusCode == 403 || statusCode == 0) {
+                Window.Location.replace(Window.Location.getPath() + "/jsp/login.jsp");
+                return;
+            }
+        }
+        callback.onFailure(error);
+    }
+
+    /**
+     * Called whenever a call to the server succeeds.  The callback that we're wrapping deals with all successful
+     * calls.
+     * 
+     * @param response the response from the server.
+     */
+    @Override
+    public void onSuccess(T response) {
+        callback.onSuccess(response);
+    }
+
+}
---
+++ src/main/java/org/iplantc/iptol/client/services/SessionManagementServiceAsync.java
@@ -0,0 +1,19 @@
+package org.iplantc.iptol.client.services;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+/**
+ * 
+ * @author sriram
+ *
+ */
+public interface SessionManagementServiceAsync {
+
+	void getAttribute(String key, AsyncCallback<Object> callback);
+
+	void setAttribute(String key, Object value, AsyncCallback<Void> callback);
+
+	void removeAttribute(String key, AsyncCallback<Void> callback);
+
+	void invalidate(AsyncCallback<Void> callback);
+
+}
