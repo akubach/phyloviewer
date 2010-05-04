ef7a081469967ae9cf028a177d339197ebbcb464
lenards <lenards@iplantcollaborative.org>
2010-05-04 14:20:38 -0700

    Remove ApplicationStatusBar and dependent events.

--- src/main/java/org/iplantc/de/client/events/DataBrowserNodeClickEventHandler.java
+++
@@ -1,8 +0,0 @@
-package org.iplantc.de.client.events;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface DataBrowserNodeClickEventHandler extends EventHandler 
-{
-	void onNodeClick(DataBrowserNodeClickEvent dbnce);	
-}
