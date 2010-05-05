fa13caa11897d396b8bd6c4a0ab218284985b42b
lenards <lenards@iplantcollaborative.org>
2010-05-05 11:13:44 -0700

    Test new Eclipse code formatting specification.

--- src/main/java/org/iplantc/de/client/utils/JsonConverter.java
+++ src/main/java/org/iplantc/de/client/utils/JsonConverter.java
@@ -2,113 +2,113 @@ package org.iplantc.de.client.utils;
 
 import java.util.List;
 
-public class JsonConverter 
+public class JsonConverter
 {
-	//////////////////////////////////////////
-	//private methods
+	// ////////////////////////////////////////
+	// private methods
 	private static String addQuotes(String in)
 	{
 		String ret = "'";
-		
+
 		if(in != null)
 		{
 			ret += in;
 		}
-		
+
 		ret += "'";
-		
+
 		return ret;
 	}
-	
-	//////////////////////////////////////////
-	private static String addIdToJson(String tag,String id)
+
+	// ////////////////////////////////////////
+	private static String addIdToJson(String tag, String id)
 	{
 		String ret = new String();
 
 		if(tag != null && id != null)
 		{
-			ret = addQuotes(tag) + ":[" + addQuotes(id) +  ']';
+			ret = addQuotes(tag) + ":[" + addQuotes(id) + ']';
 		}
-		
+
 		return ret;
 	}
-	
-	//////////////////////////////////////////
-	private static String addIdsToJson(String tag,List<String> ids)
+
+	// ////////////////////////////////////////
+	private static String addIdsToJson(String tag, List<String> ids)
 	{
-		String ret = new String();  //by default we return an empty string
-		
+		String ret = new String(); // by default we return an empty string
+
 		if(tag != null && ids != null)
 		{
 			if(ids.size() > 0)
 			{
 				ret = addQuotes(tag) + ":[";
-				
+
 				for(String item : ids)
 				{
 					ret += addQuotes(item);
 					ret += ", ";
 				}
-				
-				//trim trailing comma and space
-				ret = ret.substring(0,ret.length() - 2);
-				ret +=  ']';
-			}			
+
+				// trim trailing comma and space
+				ret = ret.substring(0, ret.length() - 2);
+				ret += ']';
+			}
 		}
-		
+
 		return ret;
 	}
 
-	//////////////////////////////////////////
-	public static String buildDeleteString(List<String> idFolders,List<String> idFiles)
+	// ////////////////////////////////////////
+	public static String buildDeleteString(List<String> idFolders, List<String> idFiles)
 	{
 		String ret = null;
-		
+
 		if(idFolders != null && idFiles != null)
 		{
 			if(idFolders.size() > 0 || idFiles.size() > 0)
 			{
 				ret = "{";
-				
-				ret += addIdsToJson("fileIds",idFiles); 
-				
-				//do we need to append a comma?
-				if(idFiles.size()  > 0 && idFolders.size() > 0)
-				{					
+
+				ret += addIdsToJson("fileIds", idFiles);
+
+				// do we need to append a comma?
+				if(idFiles.size() > 0 && idFolders.size() > 0)
+				{
 					ret += ", ";
 				}
-				
-				ret += addIdsToJson("folderIds",idFolders);
-				ret += "}";					
+
+				ret += addIdsToJson("folderIds", idFolders);
+				ret += "}";
 			}
 		}
-		
+
 		return ret;
 	}
-	
-	//////////////////////////////////////////
+
+	// ////////////////////////////////////////
 	public static String buildDeleteFolderString(String id)
 	{
 		String ret = null;
-		
-		if(id != null  && id.length() > 0)
+
+		if(id != null && id.length() > 0)
 		{
-			ret = "{" + addIdToJson("folderIds",id) + "}";					
-		}	
-		
+			ret = "{" + addIdToJson("folderIds", id) + "}";
+		}
+
 		return ret;
 	}
-	
-	//////////////////////////////////////////
+
+	// ////////////////////////////////////////
 	public static String buildDeleteFileString(String id)
 	{
 		String ret = null;
-		
+
 		if(id != null && id.length() > 0)
 		{
-			ret = "{" + addIdToJson("fileIds",id) + "}";					
-		}	
-		
+			ret = "{" + addIdToJson("fileIds", id) + "}";
+		}
+
 		return ret;
-	}	
+	}
 }
