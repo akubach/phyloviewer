74b6fbc24e165326812b07bc2d75f64717e54b5f
lenards <lenards@iplantcollaborative.org>
2010-05-10 12:30:18 -0700

    Add documentation to test entry point.

--- src/main/java/org/iplantc/phyloviewer/client/PhyloViewerTest.java
+++ src/main/java/org/iplantc/phyloviewer/client/PhyloViewerTest.java
@@ -2,6 +2,9 @@ package org.iplantc.phyloviewer.client;
 
 import com.google.gwt.core.client.EntryPoint;
 
+/**
+ * Entry point for test inclusion of GWTPhyloViewer module. 
+ */
 public class PhyloViewerTest implements EntryPoint
 {
 	@Override
