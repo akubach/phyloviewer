4067fc745210de7365a27dbd03aae765fb706cfd
akubach <adamk@tacc.utexas.edu>
2010-05-14 14:49:48 -0500

    Changing order of matrix multiplication for better zooming.

--- src/main/java/org/iplantc/phyloviewer/client/tree/viewer/render/Camera.java
+++ src/main/java/org/iplantc/phyloviewer/client/tree/viewer/render/Camera.java
@@ -28,7 +28,9 @@ public class Camera {
 		Matrix33 T0 = Matrix33.makeTranslate(0.0, 0.5);
 		Matrix33 S = Matrix33.makeScale(1, Math.pow(2, amount));
 		Matrix33 T1 = Matrix33.makeTranslate(0.0, -0.5);
-		_matrix = _matrix.multiply(T0.multiply(S.multiply(T1)));
+		
+		Matrix33 delta = T0.multiply(S.multiply(T1));
+		_matrix = delta.multiply(_matrix);
 	}
 	
 	public void panY ( double amount) {
