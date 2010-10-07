cfd583f13446c864236095816c4db9fec9dacfb7
Kristopher Urie <kurie@fieldmuseum.org>
2010-10-07 13:04:32 -0500

    updating junit tests for remote node service

---
+++ src/test/java/org/iplantc/phyloviewer/server/TestJavaTreeData.java
@@ -0,0 +1,63 @@
+package org.iplantc.phyloviewer.server;
+
+import static org.junit.Assert.*;
+
+import org.iplantc.phyloviewer.client.tree.viewer.model.Tree;
+import org.iplantc.phyloviewer.client.tree.viewer.model.remote.RemoteNode;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestJavaTreeData
+{
+	JavaTreeData treeData;
+	RemoteNode child0;
+	RemoteNode child1;
+	RemoteNode parent;
+	Tree tree;
+
+	@Before
+	public void setUp()
+	{
+		treeData = new JavaTreeData();
+		
+		child0 = new RemoteNode("child0ID", "", 1, 1, 0, new RemoteNode[0]);
+		child1 = new RemoteNode("child1ID", "", 1, 1, 0, new RemoteNode[0]);
+		RemoteNode[] children = new RemoteNode[] { child0, child1 };
+		parent = new RemoteNode("parentID", "", 3, 2, 1, children);
+
+		treeData.addRemoteNode(parent);
+		treeData.addRemoteNode(child0);
+		treeData.addRemoteNode(child1);
+
+		tree = new Tree();
+		tree.setId("treeID");
+		tree.setRootNode(parent);
+		treeData.addTree(tree);
+	}
+
+	@Test
+	public void testGetChildren()
+	{
+		RemoteNode[] returnedChildren = treeData.getChildren("parentID");
+		assertNotNull(returnedChildren);
+		assertEquals(2, returnedChildren.length);
+		assertEquals(child0, returnedChildren[0]);
+		assertEquals(child1, returnedChildren[1]);
+
+		returnedChildren = treeData.getChildren("child0ID");
+		assertNotNull(returnedChildren);
+		assertEquals(0, returnedChildren.length);
+
+		returnedChildren = treeData.getChildren("child1ID");
+		assertNotNull(returnedChildren);
+		assertEquals(0, returnedChildren.length);
+	}
+
+	@Test
+	public void testGetTree()
+	{
+		Tree returnedTree = treeData.getTree("treeID", 0);
+		assertEquals(tree, returnedTree);
+	}
+
+}
--- src/test/java/org/iplantc/phyloviewer/server/TestRemoteNodeServiceImpl.java
+++ src/test/java/org/iplantc/phyloviewer/server/TestRemoteNodeServiceImpl.java
@@ -1,156 +1,41 @@
 package org.iplantc.phyloviewer.server;
 
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Set;
-import java.util.UUID;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.Servlet;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-
 import junit.framework.TestCase;
 
-import org.iplantc.phyloviewer.client.FetchTree;
 import org.iplantc.phyloviewer.client.tree.viewer.model.Tree;
 import org.iplantc.phyloviewer.client.tree.viewer.model.remote.RemoteNode;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Test;
 
-/**
- * Tests RemoteNodeService servlet implementation.  (Just a JUnit test.  Runs faster than GwtTestRemoteNodeService).
- */
-public class TestRemoteNodeServiceImpl extends TestCase {
+public class TestRemoteNodeServiceImpl extends TestCase
+{
 	Tree tree;
 	RemoteNode root;
-	
-	@Test 
-	public void testMapSubtree() throws JSONException {
+
+	@Test
+	public void testMapSubtree() throws JSONException
+	{
 		String json = "{\"name\":\"foo\"}";
 		JSONObject obj = new JSONObject(json);
 		RemoteNode node = RemoteNodeServiceImpl.mapSubtree(obj);
-		
+
 		assertEquals("foo", node.getLabel());
 		assertEquals(1, node.getNumberOfLeafNodes());
 		assertEquals(0, node.findMaximumDepthToLeaf());
 		assertEquals(0, node.getNumberOfChildren());
 		assertEquals(0, node.getChildren().length);
 		assertTrue(node.isLeaf());
-		
-		json = "{\"children\":[" +
-					"{\"name\":\"bar\"}," +
-					"{\"name\":\"baz\"}" +
-				"]}";
+
+		json = "{\"children\":[" + "{\"name\":\"bar\"}," + "{\"name\":\"baz\"}" + "]}";
 		obj = new JSONObject(json);
 		node = RemoteNodeServiceImpl.mapSubtree(obj);
-		
-		assertEquals("bar", node.getLabel()); //parent takes first child's label
+
+		assertEquals("bar", node.getLabel()); // parent takes first child's label
 		assertEquals(2, node.getNumberOfLeafNodes());
 		assertEquals(1, node.findMaximumDepthToLeaf());
 		assertEquals(2, node.getNumberOfChildren());
 		assertEquals(2, node.getChildren().length);
 		assertFalse(node.isLeaf());
-	}
-	
-	@Test
-	public void testFetchTree() throws ServletException {
-		ServletConfig config = new MockServletConfig();
-		
-		RemoteNodeServiceImpl impl = new RemoteNodeServiceImpl();
-		impl.init(config);
-		
-		final String json = "{\"root\":{\"name\":\"foo\",\"children\":[" +
-			"{\"name\":\"bar\"}," +
-			"{\"name\":\"baz\"}" +
-		"]}}";
-		
-		FetchTree mockFetchTree = new FetchTree() {
-			public String fetchTree(int tree) { return json; }
-		};
-		
-		impl.setFetchTree(mockFetchTree);
-		
-		Tree tree = impl.getTree(0);
-		assertNotNull(tree);
-		assertEquals(3, tree.getNumberOfNodes());
-		RemoteNode root = (RemoteNode) tree.getRootNode();
-		assertNotNull(root);
-		assertNotNull(root.getChildren());
-		assertEquals(1, root.findMaximumDepthToLeaf());
-		assertEquals(2, root.getChildren().length);
-	}
-	
-	@Test
-	public void testGetChildren() {
-		RemoteNodeServiceImpl impl = new RemoteNodeServiceImpl();
-		
-		RemoteNode child0 = new RemoteNode(UUID.randomUUID().toString(), "bar", 1, 0, new RemoteNode[0]);
-		RemoteNode child1 = new RemoteNode(UUID.randomUUID().toString(), "baz", 1, 0, new RemoteNode[0]);
-		RemoteNode[] children = new RemoteNode[] {child0, child1};
-		RemoteNode parent = new RemoteNode(UUID.randomUUID().toString(), "foo", 2, 1, children);
-
-		impl.addRemoteNode(parent);
-		
-		RemoteNode[] fetchedChildren = impl.getChildren(parent.getUUID());
-		assertEquals(children, fetchedChildren);
-	}
-	
-	@SuppressWarnings("unchecked")
-	private class MockServletConfig implements ServletConfig {
-		ServletContext context = new MockServletContext();
-
-		@Override
-		public ServletContext getServletContext() {
-			return context;
-		}
-		
-		@Override public String getInitParameter(String arg0) {return null;}
-		@Override public Enumeration getInitParameterNames() {return null;}
-		@Override public String getServletName() { return null; }
-	}
-	
-	@SuppressWarnings("unchecked")
-	private class MockServletContext implements ServletContext {
-		HashMap<String, Object> attributes = new HashMap<String, Object>();
-		
-		@Override
-		public Object getAttribute(String arg0) {
-			return attributes.get(arg0);
-		}
-		
-		@Override
-		public void setAttribute(String arg0, Object arg1) {
-			attributes.put(arg0, arg1);
-		}
-
-		@Override public Enumeration getAttributeNames() {return null;}
-		@Override public ServletContext getContext(String arg0) {return null;}
-		@Override public String getContextPath() {return null;}
-		@Override public String getInitParameter(String arg0) {return null;}
-		@Override public Enumeration getInitParameterNames() {return null;}
-		@Override public int getMajorVersion() {return 0;}
-		@Override public String getMimeType(String arg0) {return null;}
-		@Override public int getMinorVersion() {return 0;}
-		@Override public RequestDispatcher getNamedDispatcher(String arg0) {return null;}
-		@Override public String getRealPath(String arg0) {return null;}
-		@Override public RequestDispatcher getRequestDispatcher(String arg0) {return null;}
-		@Override public URL getResource(String arg0) throws MalformedURLException {return null;}
-		@Override public InputStream getResourceAsStream(String arg0) {return null;}
-		@Override public Set getResourcePaths(String arg0) {return null;}
-		@Override public String getServerInfo() {return null;}
-		@Override public Servlet getServlet(String arg0) throws ServletException {return null;}
-		@Override public String getServletContextName() {return null;}
-		@Override public Enumeration getServletNames() {return null;}
-		@Override public Enumeration getServlets() {return null;}
-		@Override public void log(String arg0){}
-		@Override public void log(Exception arg0, String arg1){}
-		@Override public void log(String arg0, Throwable arg1){}
-		@Override public void removeAttribute(String arg0){}
 	}
 }
