506722cf232a47411d3fac129e45c5c82a78ca52
akubach <adamk@tacc.utexas.edu>
2010-10-14 12:51:14 -0500

    Removing unused imports

--- src/main/java/org/iplantc/phyloviewer/server/ParseTree.java
+++ src/main/java/org/iplantc/phyloviewer/server/ParseTree.java
@@ -10,10 +10,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.Enumeration;
 import java.util.List;
 
-import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
