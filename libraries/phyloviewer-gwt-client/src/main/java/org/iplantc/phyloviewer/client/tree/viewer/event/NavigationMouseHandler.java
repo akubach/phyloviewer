aea01bf9df4994c08e9120cff538669a144dd237
Kristopher Urie <kurie@fieldmuseum.org>
2011-01-03 16:28:19 -0600

    Making NavigationMouseHandler a subclass of BaseMouseHandler
    Also tidying up the 'back' gesture

--- libraries/phyloviewer-gwt-client/src/main/java/org/iplantc/phyloviewer/client/tree/viewer/event/NavigationMouseHandler.java
+++ libraries/phyloviewer-gwt-client/src/main/java/org/iplantc/phyloviewer/client/tree/viewer/event/NavigationMouseHandler.java
@@ -11,53 +11,33 @@ import org.iplantc.phyloviewer.shared.math.Vector2;
 import org.iplantc.phyloviewer.shared.model.INode;
 
 import com.google.gwt.dom.client.NativeEvent;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.DoubleClickEvent;
-import com.google.gwt.event.dom.client.DoubleClickHandler;
-import com.google.gwt.event.dom.client.HandlesAllMouseEvents;
-import com.google.gwt.event.dom.client.MouseDownEvent;
 import com.google.gwt.event.dom.client.MouseMoveEvent;
-import com.google.gwt.event.dom.client.MouseOutEvent;
-import com.google.gwt.event.dom.client.MouseOverEvent;
 import com.google.gwt.event.dom.client.MouseUpEvent;
 import com.google.gwt.event.dom.client.MouseWheelEvent;
 
-public class NavigationMouseHandler extends HandlesAllMouseEvents implements ClickHandler,
-		DoubleClickHandler
+public class NavigationMouseHandler extends BaseMouseHandler 
 {
-	private final DetailView view; // TODO could generalize this to View, if View implements isXPannable(), isYPannable() and getNodeAt
-	private Vector2 clickedPosition = null;
-	private int buttonDown = -1;
-	private Vector2 event0 = null;
-	private Vector2 event1 = null;
+	//TODO listen for tree changes on the view and clear nodeHistory and currentNodeShown
+	private final DetailView view;
 	private double dragThreshold = 10;
 	private Stack<INode> nodeHistory = new Stack<INode>(); // tried LinkedList here and the compiler complained that LinkedList.push() and LinkedList.pop() are undefined...
-
+	private INode currentNodeShown = null;
+	
 	public NavigationMouseHandler(DetailView view)
 	{
 		this.view = view;
 	}
 
 	@Override
-	public void onMouseDown(MouseDownEvent event)
-	{
-		Logger.getLogger("").log(Level.FINEST, "MouseDown");
-		event.preventDefault();
-
-		buttonDown = event.getNativeButton();
-		clickedPosition = new Vector2(event.getX(), event.getY());
-	}
-
-	@Override
 	public void onMouseUp(MouseUpEvent event)
 	{
-		Logger.getLogger("").log(Level.FINEST, "MouseUp");
-		event.preventDefault();
-
-		buttonDown = -1;
-		double finalDx = event.getX() - clickedPosition.getX();
-		double finalDy = event.getY() - clickedPosition.getY();
+		SavedMouseEvent downEvent = super.getCurrentMouseDownEvent(NativeEvent.BUTTON_LEFT); //getting mouse down data before super.onMouseUp(event) clears it
+		
+		super.onMouseUp(event);
+		
+		double finalDx = event.getX() - downEvent.x;
+		double finalDy = event.getY() - downEvent.y;
 		double absDx = Math.abs(finalDx);
 		double absDy = Math.abs(finalDy);
 
@@ -69,90 +49,69 @@ public class NavigationMouseHandler extends HandlesAllMouseEvents implements Cli
 		{
 			gestureY(finalDy);
 		}
-
-		clickedPosition = null;
-		event0 = null;
-		event1 = null;
 	}
 
 	@Override
 	public void onMouseMove(MouseMoveEvent event)
 	{
-		event1 = event0;
-		event0 = new Vector2(event.getX(), event.getY());
-
-		if(NativeEvent.BUTTON_LEFT == buttonDown) //event.getNativeButton always has value 1 for MouseMoveEvent.  See http://code.google.com/p/google-web-toolkit/issues/detail?id=3983
+		SavedMouseEvent downEvent = super.getCurrentMouseDownEvent(NativeEvent.BUTTON_LEFT);
+		
+		Vector2 event1 = super.getLastMousePosition(); //getting prev position before super.onMouseMove(event) updates it
+		super.onMouseMove(event);
+		Vector2 event0 = super.getLastMousePosition();
+		
+		if(downEvent != null && event0 != null && event1 != null)
 		{
-			if(event0 != null && event1 != null && clickedPosition != null)
-			{
-
 				Matrix33 M = view.getCamera().getMatrix(view.getWidth(), view.getHeight());
-				Matrix33 IM = M.inverse();
+			Matrix33 IM = M.inverse();
 
-				Vector2 p0 = IM.transform(event0);
-				Vector2 p1 = IM.transform(event1);
+			Vector2 p0 = IM.transform(event0);
+			Vector2 p1 = IM.transform(event1);
 
-				double x = view.isXPannable() ? p0.getX() - p1.getX() : 0.0;
-				double y = view.isYPannable() ? p0.getY() - p1.getY() : 0.0;
-				view.getCamera().pan(x, y);
-			}
+			double x = view.isXPannable() ? p0.getX() - p1.getX() : 0.0;
+			double y = view.isYPannable() ? p0.getY() - p1.getY() : 0.0;
+			view.getCamera().pan(x, y);
 		}
-	}
-
-	@Override
-	public void onMouseOut(MouseOutEvent event)
-	{
-		Logger.getLogger("").log(Level.FINEST, "MouseOut");
-		// TODO
-	}
-
-	@Override
-	public void onMouseOver(MouseOverEvent event)
-	{
-		Logger.getLogger("").log(Level.FINEST, "MouseOver");
-		// TODO
 	}
 
 	@Override
 	public void onMouseWheel(MouseWheelEvent event)
 	{
+		super.onMouseWheel(event);
+		
 		double amount = -event.getDeltaY() / 10.0;
 		amount = Math.pow(2, amount);
 		view.getCamera().zoom(amount);
 	}
 
 	@Override
-	public void onClick(ClickEvent event)
-	{
-		Logger.getLogger("").log(Level.FINEST, "Click");
-		event.preventDefault();
-		//TODO
-	}
-
-	@Override
 	public void onDoubleClick(DoubleClickEvent event)
 	{
-		Logger.getLogger("").log(Level.FINEST, "DoubleClick");
-		event.preventDefault();
+		super.onDoubleClick(event);
 
 		INode node = getClickedNode(event);
-		nodeHistory.push(node);
+		nodeHistory.push(currentNodeShown);
 		show(node);
 	}
-
+	
 	private void show(INode node)
 	{
-		if(node != null)
+		if (node == null)
 		{
-			if(view instanceof AnimatedView)
-			{
-				((AnimatedView)view).animateZoomToNode(node);
-			}
-			else
-			{
-				view.zoomToFitSubtree(node);
-			}
+			node = view.getTree().getRootNode();
 		}
+
+		//TODO move this into View and AnimatedView
+		if(view instanceof AnimatedView)
+		{
+			((AnimatedView)view).animateZoomToNode(node);
+		}
+		else
+		{
+			view.zoomToFitSubtree(node);
+		}
+		
+		currentNodeShown = node;
 	}
 
 	private INode getClickedNode(DoubleClickEvent event)
@@ -168,28 +127,23 @@ public class NavigationMouseHandler extends HandlesAllMouseEvents implements Cli
 		Logger.getLogger("").log(Level.FINEST, "gestureX() " + dx);
 		if(dx > 0)
 		{
-			// TODO pan right one 'step'. Where to?
+			// TODO pan right one 'step'. Where to? Keep forward history?  Go to highest child?
 		}
 		else
 		{
+			INode nodeToShow = null;
 			if(!nodeHistory.isEmpty())
 			{
-				// TODO if user is still looking at the last node (hasn't panned
-				// away) they won't really see a change on the first gesture.
-				// Check camera location first? Or just go immediately to the
-				// previously viewed node?
-				show(nodeHistory.pop());
+				nodeToShow = nodeHistory.pop();
 			}
-			else
-			{
-				show(view.getTree().getRootNode());
-			}
+			
+			show(nodeToShow);
 		}
 	}
 
 	private void gestureY(double dy)
 	{
 		Logger.getLogger("").log(Level.FINEST, "gestureY() " + dy);
-		// TODO
+		// TODO?
 	}
 }
