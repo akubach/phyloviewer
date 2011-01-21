2546bf1661fd18bfe544c8e180a2d2dc8aed5f37
akubach <adamk@tacc.utexas.edu>
2011-01-21 13:18:34 -0600

    Adding clone method.

--- libraries/phyloviewer-common/src/main/java/org/iplantc/phyloviewer/shared/math/Box2D.java
+++ libraries/phyloviewer-common/src/main/java/org/iplantc/phyloviewer/shared/math/Box2D.java
@@ -1,76 +1,134 @@
 /**
- * Copyright (c) 2009, iPlant Collaborative, Texas Advanced Computing Center
- * This software is licensed under the CC-GNU GPL version 2.0 or later.
- * License: http://creativecommons.org/licenses/GPL/2.0/
+ * Copyright (c) 2009, iPlant Collaborative, Texas Advanced Computing Center This software is licensed
+ * under the CC-GNU GPL version 2.0 or later. License: http://creativecommons.org/licenses/GPL/2.0/
  */
 
 package org.iplantc.phyloviewer.shared.math;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
 
-public class Box2D implements IsSerializable {
+public class Box2D implements IsSerializable
+{
+	private static final Vector2 DEFAULT_MIN_VALUE = new Vector2(Double.MAX_VALUE, Double.MAX_VALUE);
+	private static final Vector2 DEFAULT_MAX_VALUE = new Vector2(Double.MIN_VALUE, Double.MIN_VALUE);
 	
-	private Vector2 min = new Vector2(Double.MAX_VALUE,Double.MAX_VALUE);
-	private Vector2 max = new Vector2(Double.MIN_VALUE,Double.MIN_VALUE);
-	
-	public Box2D() {
-	}
-	
-	public Boolean valid() {
-	  return max.getX() >= min.getX() && max.getY() >= min.getY();
+	private Vector2 min = DEFAULT_MIN_VALUE;
+	private Vector2 max = DEFAULT_MAX_VALUE;
+
+	/**
+	 * Create a box with invalid min and max corners.
+	 */
+	public Box2D()
+	{
 	}
 
-	public Box2D(Vector2 min, Vector2 max) {
+	/**
+	 * Create a bounding box.
+	 * 
+	 * @param Lower left corner of box.
+	 * @param Upper right corner of box.
+	 */
+	public Box2D(Vector2 min, Vector2 max)
+	{
 		setMin(min);
 		setMax(max);
 	}
 
-	public Vector2 getMin() {
+	
+	/**
+	 * Clone this box
+	 */
+	public Box2D clone()
+	{
+		Vector2 min = this.getMin();
+		if(min == null)
+		{
+			min = DEFAULT_MIN_VALUE;
+		}
+		else
+		{
+			min = min.clone();
+		}
+		
+		Vector2 max = this.getMax();
+		if(max == null)
+		{
+			max = DEFAULT_MAX_VALUE;
+		}
+		else
+		{
+			max = max.clone();
+		}
+
+		return new Box2D(min, max);
+	}
+
+	public Boolean valid()
+	{
+		return max.getX() >= min.getX() && max.getY() >= min.getY();
+	}
+
+	public Vector2 getMin()
+	{
 		return min;
 	}
 
-	public void setMin(Vector2 min) {
-		if (min == this.max)
+	public void setMin(Vector2 min)
+	{
+		if(min == this.max)
 		{
-			//we don't want these to be the same object, or mutators with change both at once
+			// we don't want these to be the same object, or mutators with change both at once
 			min = new Vector2(min.getX(), min.getY());
 		}
-		
+
 		this.min = min;
 	}
 
-	public Vector2 getMax() {
+	public Vector2 getMax()
+	{
 		return max;
 	}
 
-	public void setMax(Vector2 max) {
-		if (max == this.min)
+	public void setMax(Vector2 max)
+	{
+		if(max == this.min)
 		{
-			//we don't want these to be the same object, or mutators with change both at once
+			// we don't want these to be the same object, or mutators with change both at once
 			max = new Vector2(max.getX(), max.getY());
 		}
-		
+
 		this.max = max;
 	}
-	
-	public void expandBy ( Vector2 v ) {
-		if ( v.getX() < min.getX() ) min.setX( v.getX() );
-	    if ( v.getX() > max.getX() ) max.setX( v.getX() );
-	
-	    if ( v.getY() < min.getY() ) min.setY ( v.getY() );
-	    if ( v.getY() > max.getY() ) max.setY ( v.getY() );
-	}
-	
-	public void expandBy ( Box2D bb ) {
-	    if ( false == bb.valid() ) return;
 
-	    if ( bb.getMin().getX() < min.getX() ) min.setX( bb.getMin().getX() );
-	    if ( bb.getMax().getX() > max.getX() ) max.setX( bb.getMax().getX() );
+	public void expandBy(Vector2 v)
+	{
+		if(v.getX() < min.getX())
+			min.setX(v.getX());
+		if(v.getX() > max.getX())
+			max.setX(v.getX());
 
-	    if ( bb.getMin().getY() < min.getY() ) min.setY ( bb.getMin().getY() );
-	    if ( bb.getMax().getY() > max.getY() ) max.setY ( bb.getMax().getY() );
+		if(v.getY() < min.getY())
+			min.setY(v.getY());
+		if(v.getY() > max.getY())
+			max.setY(v.getY());
 	}
-	
+
+	public void expandBy(Box2D bb)
+	{
+		if(false == bb.valid())
+			return;
+
+		if(bb.getMin().getX() < min.getX())
+			min.setX(bb.getMin().getX());
+		if(bb.getMax().getX() > max.getX())
+			max.setX(bb.getMax().getX());
+
+		if(bb.getMin().getY() < min.getY())
+			min.setY(bb.getMin().getY());
+		if(bb.getMax().getY() > max.getY())
+			max.setY(bb.getMax().getY());
+	}
+
 	public void expandBy(double d)
 	{
 		min.setX(min.getX() - d);
@@ -78,37 +136,44 @@ public class Box2D implements IsSerializable {
 		max.setX(max.getX() + d);
 		max.setY(max.getY() + d);
 	}
-	
+
 	// Return true if the given box intersects this one.
-	public boolean intersects ( Box2D bb ) {
-		return Math.max ( min.getX(), bb.getMin().getX() ) <= Math.min ( max.getX(), bb.getMax().getX() ) &&
-	           Math.max ( min.getY(), bb.getMin().getY() ) <= Math.min ( max.getY(), bb.getMax().getY() );
+	public boolean intersects(Box2D bb)
+	{
+		return Math.max(min.getX(), bb.getMin().getX()) <= Math.min(max.getX(), bb.getMax().getX())
+				&& Math.max(min.getY(), bb.getMin().getY()) <= Math.min(max.getY(), bb.getMax().getY());
 	}
 
-	public boolean contains(Vector2 position) {
-		return this.valid() && 
-			( position.getX() >= this.getMin().getX() && position.getX() <= this.getMax().getX() &&
-			  position.getY() >= this.getMin().getY() && position.getY() <= this.getMax().getY() );
+	public boolean contains(Vector2 position)
+	{
+		return this.valid()
+				&& (position.getX() >= this.getMin().getX() && position.getX() <= this.getMax().getX()
+						&& position.getY() >= this.getMin().getY() && position.getY() <= this.getMax()
+						.getY());
 	}
 
-	public Vector2 getCenter() {
-		double x = ( min.getX() + max.getX() ) / 2.0;
-	    double y = ( min.getY() + max.getY() ) / 2.0;
-	    return new Vector2 ( x, y );
+	public Vector2 getCenter()
+	{
+		double x = (min.getX() + max.getX()) / 2.0;
+		double y = (min.getY() + max.getY()) / 2.0;
+		return new Vector2(x, y);
 	}
-	
-	public double getWidth() {
+
+	public double getWidth()
+	{
 		return max.getX() - min.getX();
 	}
-	
-	public double getHeight() {
+
+	public double getHeight()
+	{
 		return max.getY() - min.getY();
 	}
-	
-	public String toString() {
+
+	public String toString()
+	{
 		return "[" + min.toString() + ", " + max.toString() + "]";
 	}
-	
+
 	/**
 	 * @return the box defined by the given corners (any two opposite corners of the box)
 	 */
@@ -119,7 +184,8 @@ public class Box2D implements IsSerializable {
 		return box;
 	}
 
-	public String toJSON() {
-		return "{\"min\":" + min.toJSON() +",\"max\":" + max.toJSON() +"}";
+	public String toJSON()
+	{
+		return "{\"min\":" + min.toJSON() + ",\"max\":" + max.toJSON() + "}";
 	}
 }
--- libraries/phyloviewer-common/src/main/java/org/iplantc/phyloviewer/shared/math/Vector2.java
+++ libraries/phyloviewer-common/src/main/java/org/iplantc/phyloviewer/shared/math/Vector2.java
@@ -1,82 +1,102 @@
 /**
- * Copyright (c) 2009, iPlant Collaborative, Texas Advanced Computing Center
- * This software is licensed under the CC-GNU GPL version 2.0 or later.
- * License: http://creativecommons.org/licenses/GPL/2.0/
+ * Copyright (c) 2009, iPlant Collaborative, Texas Advanced Computing Center This software is licensed
+ * under the CC-GNU GPL version 2.0 or later. License: http://creativecommons.org/licenses/GPL/2.0/
  */
 
 package org.iplantc.phyloviewer.shared.math;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
 
-public class Vector2 implements IsSerializable {
-
+public class Vector2 implements IsSerializable
+{
 	private double x = 0.0;
 	private double y = 0.0;
-	
-	public Vector2() {
+
+	public Vector2()
+	{
 	}
-	
-	public Vector2(double x, double y) {
+
+	public Vector2(double x, double y)
+	{
 		this.x = x;
 		this.y = y;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.iplantc.phyloviewer.client.tree.viewer.math.IVector2#getX()
+	public Vector2 clone()
+	{
+		return new Vector2(x, y);
+	}
+
+	/**
+	 * Get the x component
+	 * 
+	 * @return The value of the x component.
 	 */
-	public double getX() {
+	public double getX()
+	{
 		return x;
 	}
 
-	public void setX(double x) {
+	public void setX(double x)
+	{
 		this.x = x;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.iplantc.phyloviewer.client.tree.viewer.math.IVector2#getY()
+	/**
+	 * Get the y component
+	 * 
+	 * @return The value of the y component.
 	 */
-	public double getY() {
+	public double getY()
+	{
 		return y;
 	}
 
-	public void setY(double y) {
+	public void setY(double y)
+	{
 		this.y = y;
 	}
-	
-	public Vector2 add(Vector2 rhs) {
-		return new Vector2(this.getX() + rhs.getX(),this.getY() + rhs.getY());
+
+	public Vector2 add(Vector2 rhs)
+	{
+		return new Vector2(this.getX() + rhs.getX(), this.getY() + rhs.getY());
 	}
 
-	public Vector2 subtract(Vector2 nodePosition) {
-		return new Vector2 ( this.getX() - nodePosition.getX(), this.getY() - nodePosition.getY() );
+	public Vector2 subtract(Vector2 nodePosition)
+	{
+		return new Vector2(this.getX() - nodePosition.getX(), this.getY() - nodePosition.getY());
 	}
 
-	public double length() {
+	public double length()
+	{
 		return Math.sqrt((this.getX() * this.getX()) + (this.getY() * this.getY()));
 	}
-	
-	public Vector2 rotate(double angle) {
+
+	public Vector2 rotate(double angle)
+	{
 		double x = this.getX() * Math.cos(angle) - this.getY() * Math.sin(angle);
 		double y = this.getX() * Math.sin(angle) + this.getY() * Math.cos(angle);
-		return new Vector2(x,y);
+		return new Vector2(x, y);
 	}
-	
+
 	public double distance(Vector2 other)
 	{
 		return Math.sqrt(this.distanceSquared(other));
 	}
-	
+
 	public double distanceSquared(Vector2 other)
 	{
-		return ((this.getX() - other.getX()) * (this.getX() - other.getX())) +
-			((this.getY() - other.getY()) * (this.getY() - other.getY()));
+		return ((this.getX() - other.getX()) * (this.getX() - other.getX()))
+				+ ((this.getY() - other.getY()) * (this.getY() - other.getY()));
 	}
-	
-	public String toString() {
+
+	public String toString()
+	{
 		return "(" + x + "," + y + ")";
 	}
-	
-	public String toJSON() {
+
+	public String toJSON()
+	{
 		return "{\"x\":" + this.getX() + ",\"y\":" + this.getY() + "}";
 	}
 }
