38529898f0bf023c6ef6a20bd6d26e009b4a122b
akubach <adamk@tacc.utexas.edu>
2011-01-20 18:43:31 -0600

    Providing a getter and setter for the number of steps in a animation.

--- libraries/phyloviewer-gwt-client/src/main/java/org/iplantc/phyloviewer/client/tree/viewer/AnimatedView.java
+++ libraries/phyloviewer-gwt-client/src/main/java/org/iplantc/phyloviewer/client/tree/viewer/AnimatedView.java
@@ -6,48 +6,69 @@ import org.iplantc.phyloviewer.shared.render.Camera;
 
 import com.google.gwt.user.client.Timer;
 
-public abstract class AnimatedView extends View {
-
-	public AnimatedView()
-	{
-	}
-	
+public abstract class AnimatedView extends View
+{
+	private int numberOfAnimationSteps = 6;
 	private AnimateCamera animator;
-	private Timer renderTimer = new Timer() {
-		public void run() {
-			if (AnimatedView.this.isReady()) {
+	private Timer renderTimer = new Timer()
+	{
+		public void run()
+		{
+			if(AnimatedView.this.isReady())
+			{
 				renderFrame();
-			} else {
+			}
+			else
+			{
 				this.schedule(33);
 			}
 		}
 	};
+	
+	public AnimatedView()
+	{
+	}
 
-	private void renderFrame() {
-		if (animator != null) {
+	private void renderFrame()
+	{
+		if(animator != null)
+		{
 			getCamera().setViewMatrix(animator.getNextMatrix());
 
-			if (animator.isDone()) {
+			if(animator.isDone())
+			{
+				// We are done.  clear the animator and cancel the timer.
 				animator = null;
-
 				renderTimer.cancel();
 			}
 		}
-		
-		// Dispatch a render event.  This will make sure all views are updated with this camera.
+
+		// Dispatch a render event. This will make sure all views are updated with this camera.
 		this.dispatch(new RenderEvent());
 	}
 
-	protected void startAnimation(Camera finalCamera) {
-		animator = new AnimateCamera(getCamera().getViewMatrix(), finalCamera.getViewMatrix(), 25);
+	protected void startAnimation(Camera finalCamera)
+	{
+		animator = new AnimateCamera(getCamera().getViewMatrix(), finalCamera.getViewMatrix(),
+				numberOfAnimationSteps);
 		renderTimer.scheduleRepeating(30);
 	}
-	
-	public void zoomToBoundingBox(Box2D boundingBox) 
+
+	public void zoomToBoundingBox(Box2D boundingBox)
 	{
 		Camera finalCamera = getCamera().create();
 		finalCamera.zoomToBoundingBox(boundingBox);
 
 		startAnimation(finalCamera);
+	}
+
+	public int getNumberOfAnimationSteps()
+	{
+		return numberOfAnimationSteps;
+	}
+
+	public void setNumberOfAnimationSteps(int numberOfAnimationSteps)
+	{
+		this.numberOfAnimationSteps = numberOfAnimationSteps;
 	}
 }
