4c70675f9e7a580777a6298768799d61e8819f25
akubach <adamk@tacc.utexas.edu>
2011-01-21 13:17:31 -0600

    Renaming handleResponse to setPositionAndBounds.

--- libraries/phyloviewer-gwt-client/src/main/java/org/iplantc/phyloviewer/client/tree/viewer/PagedDocument.java
+++ libraries/phyloviewer-gwt-client/src/main/java/org/iplantc/phyloviewer/client/tree/viewer/PagedDocument.java
@@ -7,6 +7,7 @@ import java.util.logging.Logger;
 
 import org.iplantc.phyloviewer.client.events.RenderEvent;
 import org.iplantc.phyloviewer.client.services.CombinedService.CombinedResponse;
+import org.iplantc.phyloviewer.client.services.CombinedService.LayoutResponse;
 import org.iplantc.phyloviewer.client.services.CombinedService.NodeResponse;
 import org.iplantc.phyloviewer.client.services.CombinedServiceAsync;
 import org.iplantc.phyloviewer.client.tree.viewer.layout.remote.RemoteLayout;
@@ -18,82 +19,99 @@ import org.iplantc.phyloviewer.shared.model.Tree;
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
-public class PagedDocument extends Document {
-	
+public class PagedDocument extends Document
+{
+
 	private static Logger rootLogger = Logger.getLogger("");
 	RemoteLayout remoteLayout = new RemoteLayout();
 	CombinedServiceAsync combinedService;
 	EventBus eventBus;
 	Set<Integer> pendingRequests = new HashSet<Integer>();
 
-	public PagedDocument(CombinedServiceAsync combinedService, EventBus eventBus, int treeId, NodeResponse nodeData) {
+	public PagedDocument(CombinedServiceAsync combinedService, EventBus eventBus, int treeId,
+			NodeResponse nodeData)
+	{
 		super();
-		
+
 		this.combinedService = combinedService;
 		this.eventBus = eventBus;
-		
+
 		Tree tree = new Tree();
 		tree.setId(treeId);
 		tree.setRootNode(nodeData.node);
-		
+
 		this.setTree(tree);
-		
+
 		int numberOfNodes = nodeData.node.getNumberOfNodes();
-		
+
 		remoteLayout.init(numberOfNodes);
-		remoteLayout.handleResponse(nodeData.layout);
+		remoteLayout.setPositionAndBounds(nodeData.layout.nodeID, nodeData.layout.position,
+				nodeData.layout.boundingBox);
 		this.setLayout(remoteLayout);
 	}
-	
 
 	@Override
 	public boolean checkForData(final INode node)
 	{
 		// Return now if we are waiting for a response from the server.
-		if(pendingRequests.contains(node.getId())) {
+		if(pendingRequests.contains(node.getId()))
+		{
 			return false;
 		}
-		
+
 		final RemoteLayout rLayout = remoteLayout;
-		if (node instanceof RemoteNode) 
+		if(node instanceof RemoteNode)
 		{
-			final RemoteNode rNode = (RemoteNode) node;
-		
-			if (rNode.getChildren() == null) {
+			final RemoteNode rNode = (RemoteNode)node;
+
+			if(rNode.getChildren() == null)
+			{
 				pendingRequests.add(rNode.getId());
-				
-				rootLogger.log(Level.INFO, "RenderTree.checkForData(): Fetching layouts for children of node \"" + rNode.getLabel() + "\" (" + rNode.getId() + ").");
-				combinedService.getChildrenAndLayout(rNode.getId(), new AsyncCallback<CombinedResponse>()
-				{
 
-					@Override
-					public void onFailure(Throwable arg0) {
-						pendingRequests.remove(rNode.getId());
-					}
+				rootLogger.log(
+						Level.INFO,
+						"RenderTree.checkForData(): Fetching layouts for children of node \""
+								+ rNode.getLabel() + "\" (" + rNode.getId() + ").");
+				combinedService.getChildrenAndLayout(rNode.getId(),
+						new AsyncCallback<CombinedResponse>()
+						{
 
-					@Override
-					public void onSuccess(CombinedResponse response) {
-						pendingRequests.remove(rNode.getId());
-						
-						rNode.setChildren(response.nodes);
-						
-						for (int i = 0; i < response.layouts.length; i++) {
-							rLayout.handleResponse(response.layouts[i]);
-						}
-						
-						if (eventBus != null) {
-							rootLogger.log(Level.INFO, "Rendering: got layouts for children of node \"" + node.getLabel() + "\".");
-							eventBus.fireEvent(new RenderEvent());
-						}
-					}
-					
-				});
-	
+							@Override
+							public void onFailure(Throwable arg0)
+							{
+								pendingRequests.remove(rNode.getId());
+							}
+
+							@Override
+							public void onSuccess(CombinedResponse response)
+							{
+								pendingRequests.remove(rNode.getId());
+
+								rNode.setChildren(response.nodes);
+
+								for(LayoutResponse layoutResponse : response.layouts)
+								{
+									rLayout.setPositionAndBounds(layoutResponse.nodeID,
+											layoutResponse.position, layoutResponse.boundingBox);
+								}
+
+								if(eventBus != null)
+								{
+									rootLogger.log(
+											Level.INFO,
+											"Rendering: got layouts for children of node \""
+													+ node.getLabel() + "\".");
+									eventBus.fireEvent(new RenderEvent());
+								}
+							}
+
+						});
+
 				return false;
-				
+
 			}
 		}
-		
+
 		return true;
 	}
 }
--- libraries/phyloviewer-gwt-client/src/main/java/org/iplantc/phyloviewer/client/tree/viewer/layout/remote/RemoteLayout.java
+++ libraries/phyloviewer-gwt-client/src/main/java/org/iplantc/phyloviewer/client/tree/viewer/layout/remote/RemoteLayout.java
@@ -3,55 +3,64 @@ package org.iplantc.phyloviewer.client.tree.viewer.layout.remote;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.iplantc.phyloviewer.client.services.CombinedService.LayoutResponse;
 import org.iplantc.phyloviewer.shared.layout.ILayoutData;
 import org.iplantc.phyloviewer.shared.math.Box2D;
 import org.iplantc.phyloviewer.shared.math.Vector2;
 import org.iplantc.phyloviewer.shared.model.INode;
 
-public class RemoteLayout implements ILayoutData {
+public class RemoteLayout implements ILayoutData
+{
+	private Map<Integer,Vector2> positions = new HashMap<Integer,Vector2>();
+	private Map<Integer,Box2D> bounds = new HashMap<Integer,Box2D>();
 
-	private Map<Integer, Vector2> positions = new HashMap<Integer, Vector2>();
-	private Map<Integer, Box2D> bounds = new HashMap<Integer, Box2D>();
-
-	public RemoteLayout() {
+	public RemoteLayout()
+	{
 	}
-	
+
 	@Override
-	public Box2D getBoundingBox(INode node) {
+	public Box2D getBoundingBox(INode node)
+	{
 		return this.getBoundingBox(node.getId());
 	}
-	
+
 	@Override
-	public Box2D getBoundingBox(int nodeId) {
+	public Box2D getBoundingBox(int nodeId)
+	{
 		return bounds.get(nodeId);
 	}
 
 	@Override
-	public Vector2 getPosition(INode node) {
+	public Vector2 getPosition(INode node)
+	{
 		return positions.get(node.getId());
 	}
 
-	public boolean containsNode(INode node) {
+	public boolean containsNode(INode node)
+	{
 		return this.positions.containsKey(node.getId());
 	}
-	
-	public boolean containsNodes(INode[] nodes) {
-		for (int i = 0; i < nodes.length; i++) {
-			if (!this.containsNode(nodes[i])) {
+
+	public boolean containsNodes(INode[] nodes)
+	{
+		for(int i = 0;i < nodes.length;i++)
+		{
+			if(!this.containsNode(nodes[i]))
+			{
 				return false;
 			}
 		}
 		return true;
 	}
 
-	public void init(int numberOfNodes) {
-		positions = new HashMap<Integer, Vector2>(numberOfNodes);
-		bounds = new HashMap<Integer, Box2D>(numberOfNodes);
+	public void init(int numberOfNodes)
+	{
+		positions = new HashMap<Integer,Vector2>(numberOfNodes);
+		bounds = new HashMap<Integer,Box2D>(numberOfNodes);
 	}
-	
-	public void handleResponse(LayoutResponse response) {
-		bounds.put(response.nodeID, response.boundingBox);
-		positions.put(response.nodeID, response.position);
+
+	public void setPositionAndBounds(int nodeId, Vector2 position, Box2D box)
+	{
+		bounds.put(nodeId, box);
+		positions.put(nodeId, position);
 	}
 }
