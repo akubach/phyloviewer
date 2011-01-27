5bfc848ca52ad43e70ff2504f45cb268ed026f50
akubach <adamk@tacc.utexas.edu>
2011-01-27 09:52:39 -0600

    Fixing code style.

--- libraries/phyloviewer-common/src/main/java/org/iplantc/phyloviewer/shared/scene/BranchBuilderCladogram.java
+++ libraries/phyloviewer-common/src/main/java/org/iplantc/phyloviewer/shared/scene/BranchBuilderCladogram.java
@@ -5,23 +5,23 @@ import org.iplantc.phyloviewer.shared.math.Box2D;
 import org.iplantc.phyloviewer.shared.math.Vector2;
 import org.iplantc.phyloviewer.shared.model.INode;
 
-public class BranchBuilderCladogram implements IBranchBuilder {
-
+public class BranchBuilderCladogram implements IBranchBuilder
+{
 	@Override
-	public Drawable[] buildBranch(INode parent, INode child, ILayoutData layout) {
-		
+	public Drawable[] buildBranch(INode parent, INode child, ILayoutData layout)
+	{
 		Vector2 start = layout.getPosition(parent);
 		Vector2 end = layout.getPosition(child);
-		
+
 		Vector2 vertices[] = new Vector2[3];
 		vertices[0] = start;
-		vertices[1] = new Vector2(start.getX(),end.getY());
+		vertices[1] = new Vector2(start.getX(), end.getY());
 		vertices[2] = end;
 
 		Box2D box = new Box2D();
 		box.expandBy(start);
 		box.expandBy(end);
-		Drawable[] drawables = new Drawable[] { new Line(vertices,box) };
+		Drawable[] drawables = new Drawable[] { new Line(vertices, box) };
 		return drawables;
 	}
 }
--- libraries/phyloviewer-common/src/main/java/org/iplantc/phyloviewer/shared/scene/IBranchBuilder.java
+++ libraries/phyloviewer-common/src/main/java/org/iplantc/phyloviewer/shared/scene/IBranchBuilder.java
@@ -3,6 +3,7 @@ package org.iplantc.phyloviewer.shared.scene;
 import org.iplantc.phyloviewer.shared.layout.ILayoutData;
 import org.iplantc.phyloviewer.shared.model.INode;
 
-public interface IBranchBuilder {
+public interface IBranchBuilder
+{
 	public Drawable[] buildBranch(INode parent, INode child, ILayoutData layout);
 }
