34b8c9db1d47e5765fe32d9db4fb269327eece13
akubach <adamk@tacc.utexas.edu>
2011-02-05 22:40:59 -0600

    Adding javadoc comments.

--- libraries/phyloviewer-common/src/main/java/org/iplantc/phyloviewer/shared/model/IDocument.java
+++ libraries/phyloviewer-common/src/main/java/org/iplantc/phyloviewer/shared/model/IDocument.java
@@ -4,20 +4,56 @@ import org.iplantc.phyloviewer.shared.layout.ILayoutData;
 import org.iplantc.phyloviewer.shared.render.style.IStyle;
 import org.iplantc.phyloviewer.shared.render.style.IStyleMap;
 
-public interface IDocument {
-
+public interface IDocument
+{
+	/**
+	 * Get the tree.
+	 * @return
+	 */
 	public abstract ITree getTree();
 
+	/**
+	 * Get the style map.
+	 * @return
+	 */
 	public abstract IStyleMap getStyleMap();
+
+	/**
+	 * Set the style map.
+	 * @param styleMap
+	 */
 	public abstract void setStyleMap(IStyleMap styleMap);
 
+	/**
+	 * Get the style for the node.
+	 * @param node
+	 * @return
+	 */
 	public abstract IStyle getStyle(INode node);
-	
+
+	/**
+	 * Get the lable to use for the node.
+	 * @param node
+	 * @return
+	 */
 	public abstract String getLabel(INode node);
-	
+
+	/**
+	 * Get the layout data.
+	 * @return
+	 */
 	public abstract ILayoutData getLayout();
-	
+
+	/**
+	 * Is all the data for the node local?
+	 * @param node
+	 * @return
+	 */
 	public abstract boolean checkForData(final INode node);
-	
+
+	/**
+	 * Is the document ready to begin rendering?
+	 * @return
+	 */
 	public abstract boolean isReady();
 }
