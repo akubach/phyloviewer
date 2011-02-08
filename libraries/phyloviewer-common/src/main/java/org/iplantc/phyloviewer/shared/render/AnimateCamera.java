aa3e9e16fc0cd27fcea24d573f9a76f0905c253c
akubach <adamk@tacc.utexas.edu>
2011-02-08 17:20:50 -0600

    Updating formating

--- libraries/phyloviewer-common/src/main/java/org/iplantc/phyloviewer/shared/render/AnimateCamera.java
+++ libraries/phyloviewer-common/src/main/java/org/iplantc/phyloviewer/shared/render/AnimateCamera.java
@@ -1,46 +1,51 @@
 /**
- * Copyright (c) 2009, iPlant Collaborative, Texas Advanced Computing Center
- * This software is licensed under the CC-GNU GPL version 2.0 or later.
- * License: http://creativecommons.org/licenses/GPL/2.0/
+ * Copyright (c) 2009, iPlant Collaborative, Texas Advanced Computing Center This software is licensed
+ * under the CC-GNU GPL version 2.0 or later. License: http://creativecommons.org/licenses/GPL/2.0/
  */
 
 package org.iplantc.phyloviewer.shared.render;
 
 import org.iplantc.phyloviewer.shared.math.Matrix33;
 
-public class AnimateCamera {
-
+public class AnimateCamera
+{
 	Matrix33 initialMatrix;
 	Matrix33 finalMatrix;
 	double u;
 	double stepSize;
-	
-	public AnimateCamera(Matrix33 initialCamera, Matrix33 finalCamera, int numberOfSteps) {
+
+	public AnimateCamera(Matrix33 initialCamera, Matrix33 finalCamera, int numberOfSteps)
+	{
 		this.initialMatrix = initialCamera;
 		this.finalMatrix = finalCamera;
 		this.u = 0.0;
-		this.stepSize = ( 0 != numberOfSteps ? 1.0 / numberOfSteps : 1 );
+		this.stepSize = (0 != numberOfSteps ? 1.0 / numberOfSteps : 1);
 	}
-	
-	public boolean isDone() {
+
+	public boolean isDone()
+	{
 		return u >= 1.0;
 	}
-	
-	public Matrix33 getNextMatrix() {
-		if(this.initialMatrix == null || this.finalMatrix == null) {
+
+	public Matrix33 getNextMatrix()
+	{
+		if(this.initialMatrix == null || this.finalMatrix == null)
+		{
 			return new Matrix33();
 		}
-		
-		u = Math.min( u + stepSize, 1.0 );
+
+		u = Math.min(u + stepSize, 1.0);
 
 		Matrix33 result = new Matrix33();
-		for ( int i = 0; i < 3; ++i ) {
-			for ( int j = 0; j < 3; ++j ) {
-				double difference = finalMatrix.get(i, j) - initialMatrix.get(i,j);
-				result.set(i, j, initialMatrix.get(i, j) + ( u * difference ) );
+		for(int i = 0;i < 3;++i)
+		{
+			for(int j = 0;j < 3;++j)
+			{
+				double difference = finalMatrix.get(i, j) - initialMatrix.get(i, j);
+				result.set(i, j, initialMatrix.get(i, j) + (u * difference));
 			}
 		}
-		
+
 		return result;
 	}
 }
