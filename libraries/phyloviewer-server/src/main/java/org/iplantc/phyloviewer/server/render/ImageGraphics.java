500559b6d07e3ebbab6bbf548db86ad4c5025013
akubach <adamk@tacc.utexas.edu>
2011-02-15 10:59:19 -0600

    Turning on anti-aliasing.

--- libraries/phyloviewer-server/src/main/java/org/iplantc/phyloviewer/server/render/ImageGraphics.java
+++ libraries/phyloviewer-server/src/main/java/org/iplantc/phyloviewer/server/render/ImageGraphics.java
@@ -2,38 +2,47 @@ package org.iplantc.phyloviewer.server.render;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 
 import org.iplantc.phyloviewer.shared.math.Box2D;
 import org.iplantc.phyloviewer.shared.math.Vector2;
 
-public class ImageGraphics extends Java2DGraphics {
-
+public class ImageGraphics extends Java2DGraphics
+{
 	private BufferedImage image;
 	private Box2D screenBounds = new Box2D();
-	
-	public ImageGraphics(int width, int height) {
+
+	public ImageGraphics(int width, int height)
+	{
 		super();
-		
+
 		image = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
 		Graphics2D g2d = image.createGraphics();
+
+		// Turn on anti-aliasing.
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
 		g2d.setClip(0, 0, width, height);
 		g2d.setBackground(new Color(0.0f, 0.0f, 0.0f, 1.0f));
 		this.setGraphics2D(g2d);
-		
-		screenBounds.setMin(new Vector2(0,0));
-		screenBounds.setMax(new Vector2(width,height));
-		
-		this.setAffineTransform(AffineTransform.getScaleInstance(width-1, height-1)); //subtracting a pixel here to make sure lines on the bottom row get drawn
+
+		screenBounds.setMin(new Vector2(0, 0));
+		screenBounds.setMax(new Vector2(width, height));
+
+		// subtracting a pixel here to make sure lines on the bottom row get drawn
+		this.setAffineTransform(AffineTransform.getScaleInstance(width - 1, height - 1));
 	}
-	
-	public BufferedImage getImage() {
+
+	public BufferedImage getImage()
+	{
 		return image;
 	}
-	
-	public Boolean isCulled(Box2D bbox) {
-		if ( !bbox.valid() )
+
+	public Boolean isCulled(Box2D bbox)
+	{
+		if(!bbox.valid())
 			return false;
 
 		return !screenBounds.intersects(bbox);
